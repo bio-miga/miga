968035c71c06e8b75f52522c5abc37e55482895e
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-06-29 19:24:07 -0400

    clade_finding escape problems in cluster arch

--- scripts/clade_finding.bash
+++ scripts/clade_finding.bash
@@ -10,9 +10,9 @@ date "+%Y-%m-%d %H:%M:%S %z" > "miga.project.start"
 
 # Markov-cluster genomes by ANI
 gunzip -c ../09.distances/03.ani/miga.project.txt.gz | tail -n+2 | awk -F'\t' '{print $2"\t"$3"\t"$4}' > genome-genome.aai90.rbm
-ogs.mcl.rb -d . -o miga-project.ani-clades -t "$CORES" -i -f '(\S+)-(\S+)\.aai90\.rbm'
+ogs.mcl.rb -d . -o miga-project.ani-clades -t "$CORES" -i -f "(\\S+)-(\\S+)\\.aai90\\.rbm"
 cat genome-genome.rbm | awk -F'\t' '$3>=95' > genome-genome.ani95.rbm
-ogs.mcl.rb -d . -o miga-project.ani95-clades -t "$CORES" -b -f '(\S+)-(\S+)\.ani95\.rbm'
+ogs.mcl.rb -d . -o miga-project.ani95-clades -t "$CORES" -b -f "(\\S+)-(\\S+)\\.ani95\\.rbm"
 
 # Propose clade projects
 cat miga-project.ani95-clades | tail -n +2 | tr ',' '\t' | awk 'NF >= 5' > miga-project.proposed-clades
