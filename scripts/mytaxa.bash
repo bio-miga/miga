fdc7b37d62e8e5ae64090126370afc58070862cb
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-07-02 14:11:48 -0400

    Small bug fixed in mytaxa

--- scripts/mytaxa.bash
+++ scripts/mytaxa.bash
@@ -15,8 +15,8 @@ MT=$(dirname -- $(which MyTaxa))
 MULTI=$($MIGA/bin/list_datasets -P "$PROJECT" -D "$DATASET" --multi | wc -l | awk '{print $1}')
 if [[ "$MULTI" -eq "1" ]] ; then
    # Check requirements
-   if [[ ! -e "$MT/AllGenomes.faa.pal" ]] ; then
-      echo "Cannot locate the database: $MT/AllGenomes.faa.pal: no such file or directory" >&2
+   if [[ ! -e "$MT/AllGenomes.faa.dmnd" ]] ; then
+      echo "Cannot locate the database: $MT/AllGenomes.faa.dmnd: no such file or directory" >&2
       exit 1
    fi
    if [[ ! -d "$MT/db" ]] ; then
