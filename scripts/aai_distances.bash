d5cb8d806d01e1b49c778f2de7365af8801a6376
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-07-04 16:52:07 -0400

    *_distances can now be executed through multiple runs This allows large projects to be completed even with walltime restrictions in place

--- scripts/aai_distances.bash
+++ scripts/aai_distances.bash
@@ -8,35 +8,39 @@ cd "$PROJECT/data/09.distances/02.aai"
 # Initialize
 date "+%Y-%m-%d %H:%M:%S %z" > "miga-project.start"
 
-echo "metric	a	b	value	sd	n	omega" > "miga-project.txt.tmp"
 echo -n "" > "miga-project.log"
 DS=$($MIGA/bin/list_datasets -P "$PROJECT" --ref --no-multi)
 for i in $DS ; do
-   # Check if this is done (e.g., in a previous failed iteration)
-   if [[ ! -s "../$i.json" ]] ; then
-      echo "$i: Incomplete job, aborting project-wide update..." >&2
-      exit 1
-   fi
-   [[ -d "$i.d" ]] || continue
-   
-   # Concatenate results
-   for j in $DS ; do
-      [[ "$i" == "$j" ]] && break # Only lower triangle
-      if [[ -e "$i.d/$j.txt" ]] ; then
-	 cat "$i.d/$j.txt" >> "miga-project.txt.tmp"
-      elif [[ -e "$j.d/$i.txt" ]] ; then
-	 cat "$j.d/$i.txt" >> "miga-project.txt.tmp"
-      else
-	 continue # Ignore missing data
+   if [[ ! -s "miga-project.$DS.txt" ]] ; then
+      # Check if this dataset is done (e.g., in a previous failed iteration)
+      if [[ ! -s "../$i.json" ]] ; then
+	 echo "$i: Incomplete job, aborting project-wide update..." >&2
+	 exit 1
       fi
-      echo "" >> "miga-project.txt.tmp"
-   done
+      [[ -d "$i.d" ]] || continue
+      
+      # Concatenate results
+      [[ -e "miga-project.$DS.txt.tmp" ]] && rm "miga-project.$DS.txt.tmp"
+      for j in $DS ; do
+	 [[ "$i" == "$j" ]] && break # Only lower triangle
+	 if [[ -e "$i.d/$j.txt" ]] ; then
+	    cat "$i.d/$j.txt" >> "miga-project.$DS.txt.tmp"
+	 elif [[ -e "$j.d/$i.txt" ]] ; then
+	    cat "$j.d/$i.txt" >> "miga-project.$DS.txt.tmp"
+	 else
+	    continue # Ignore missing data
+	 fi
+      done
+      mv "miga-project.$DS.txt.tmp" "miga-project.$DS.txt"
+   fi
    echo "$i" >> "miga-project.log"
 done
 
+# Merge
+echo "metric	a	b	value	sd	n	omega" > "miga-project.txt"
+cat "miga-project.*.txt" | grep . | perl -pe "s/ /\\t/g" >> "miga-project.txt"
+rm "miga-project.*.txt"
 
-cat "miga-project.txt.tmp" | grep . | perl -pe "s/ /\\t/g" > "miga-project.txt"
-rm "miga-project.txt.tmp"
 # R-ify
 echo "
 aai <- read.table('miga-project.txt', sep='\\t', h=T)
@@ -44,7 +48,7 @@ save(aai, file='miga-project.Rdata')
 " | R --vanilla
 
 # Gzip
-gzip miga-project.txt
+gzip -9 -f miga-project.txt
 
 # Finalize
 date "+%Y-%m-%d %H:%M:%S %z" > "miga-project.done"
--- scripts/haai_distances.bash
+++ scripts/haai_distances.bash
@@ -8,34 +8,39 @@ cd "$PROJECT/data/09.distances/01.haai"
 # Initialize
 date "+%Y-%m-%d %H:%M:%S %z" > "miga-project.start"
 
-echo "metric	a	b	value	sd	n	omega" > "miga-project.txt.tmp"
 echo -n "" > "miga-project.log"
 DS=$($MIGA/bin/list_datasets -P "$PROJECT" --ref --no-multi)
 for i in $DS ; do
-   # Check if this is done (e.g., in a previous failed iteration)
-   if [[ ! -s "../$i.json" ]] ; then
-      echo "$i: Incomplete job, aborting project-wide update..." >&2
-      exit 1
-   fi
-   [[ -d "$i.d" ]] || continue
-   
-   # Concatenate results
-   for j in $DS ; do
-      [[ "$i" == "$j" ]] && break # Only lower triangle
-      if [[ -e "$i.d/$j.txt" ]] ; then
-	 cat "$i.d/$j.txt" >> "miga-project.txt.tmp"
-      elif [[ -e "$j.d/$i.txt" ]] ; then
-	 cat "$j.d/$i.txt" >> "miga-project.txt.tmp"
-      else
-	 continue # Ignore missing data
+   if [[ ! -s "miga-project.$DS.txt" ]] ; then
+      # Check if this dataset is done (e.g., in a previous failed iteration)
+      if [[ ! -s "../$i.json" ]] ; then
+	 echo "$i: Incomplete job, aborting project-wide update..." >&2
+	 exit 1
       fi
-      echo "" >> "miga-project.txt.tmp"
-   done
+      [[ -d "$i.d" ]] || continue
+      
+      # Concatenate results
+      [[ -e "miga-project.$DS.txt.tmp" ]] && rm "miga-project.$DS.txt.tmp"
+      for j in $DS ; do
+	 [[ "$i" == "$j" ]] && break # Only lower triangle
+	 if [[ -e "$i.d/$j.txt" ]] ; then
+	    cat "$i.d/$j.txt" >> "miga-project.$DS.txt.tmp"
+	 elif [[ -e "$j.d/$i.txt" ]] ; then
+	    cat "$j.d/$i.txt" >> "miga-project.$DS.txt.tmp"
+	 else
+	    continue # Ignore missing data
+	 fi
+      done
+      mv "miga-project.$DS.txt.tmp" "miga-project.$DS.txt"
+   fi
    echo "$i" >> "miga-project.log"
 done
 
-cat "miga-project.txt.tmp" | grep . | perl -pe "s/ /\\t/g" > "miga-project.txt"
-rm "miga-project.txt.tmp"
+# Merge
+echo "metric	a	b	value	sd	n	omega" > "miga-project.txt"
+cat "miga-project.*.txt" | grep . | perl -pe "s/ /\\t/g" >> "miga-project.txt"
+rm "miga-project.*.txt"
+
 # R-ify
 echo "
 haai <- read.table('miga-project.txt', sep='\\t', h=T)
