d1ace218f7307dac24ec12ec6bb52f4060e4ae9d
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-08-18 17:34:12 -0400

    *_distances.bash migrated to SQLite3

--- scripts/ani_distances.bash
+++ scripts/ani_distances.bash
@@ -10,39 +10,14 @@ date "+%Y-%m-%d %H:%M:%S %z" > "miga-project.start"
 
 echo -n "" > "miga-project.log"
 DS=$($MIGA/bin/list_datasets -P "$PROJECT" --ref --no-multi)
-for i in $DS ; do
-   if [[ ! -s "miga-project.$i.txt" ]] ; then
-      # Check if this dataset is done (e.g., in a previous failed iteration)
-      if [[ ! -s "../$i.json" ]] ; then
-	 echo "$i: Incomplete job, aborting project-wide update..." >&2
-	 exit 1
-      fi
-      [[ -d "$i.d" ]] || continue
-      
-      # Concatenate results
-      [[ -e "miga-project.$i.txt.tmp" ]] && rm "miga-project.$i.txt.tmp"
-      touch "miga-project.$i.txt.tmp"
-      for j in $DS ; do
-	 [[ "$i" == "$j" ]] && break # Only lower triangle
-	 if [[ -e "$i.d/$j.txt" ]] ; then
-	    cat "$i.d/$j.txt" >> "miga-project.$i.txt.tmp"
-	 elif [[ -e "$j.d/$i.txt" ]] ; then
-	    cat "$j.d/$i.txt" >> "miga-project.$i.txt.tmp"
-	 else
-	    continue # Ignore missing data
-	 fi
-      done
-      mv "miga-project.$i.txt.tmp" "miga-project.$i.txt"
-   fi
-   echo "$i" >> "miga-project.log"
-done
 
-# Merge
 (
 echo "metric	a	b	value	sd	n	omega"
-cat miga-project.*.txt | grep .
-) | perl -pe "s/ /\\t/g" >> "miga-project.txt"
-rm miga-project.*.txt
+for i in $DS ; do
+   echo "select * from ani;" | sqlite3 $i.db
+   echo "$i" >> "miga-project.log"
+done | tr "\\|" "\\t"
+) > "miga-project.txt"
 
 # R-ify
 echo "
