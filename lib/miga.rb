672dadb6464064936fcef6fe6ecef0a68abfcaa2
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-11-04 00:46:08 -0500

    v0.2.1 Recent changes in a gem

--- lib/miga.rb
+++ lib/miga.rb
@@ -12,9 +12,9 @@ require "miga/project"
 require "miga/taxonomy"
 
 module MiGA
-   VERSION = [0.2, 0, 0]
+   VERSION = [0.2, 0, 1]
    VERSION_NAME = "pochoir"
-   VERSION_DATE = Date.new(2015, 10, 1)
+   VERSION_DATE = Date.new(2015, 11, 4)
    CITATION = "Rodriguez-R et al, in preparation. Microbial Genomes Atlas: " +
       "Standardizing genomic and metagenomic analyses for Archaea and Bacteria."
    class MiGA
