41849610f8493073be321b95b40fe09057e70069
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-11-14 19:34:59 -0500

    distance helpers Decentralized cluttery code. Distances now differentiate between refs and non-refs

---
+++ scripts/_distances_noref_nomulti.bash
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Available variables: $PROJECT, $DATASET, $RUNTYPE, $MIGA, $CORES, $TMPDIR,
+# 			$NOMULTI, $REF
+
+# Deal with previous runs (if any)
+exists $DATASET.a[an]i.db && cp $DATASET.a[an]i.db $TMPDIR
+exists $DATASET.a[an]i.9[05] && rm $DATASET.a[an]i.9[05]
+N=0
+function checkpoint_n {
+   let N=$N+1
+   if [[ $N -ge 10 ]] ; then
+      cp $TMPDIR/$DATASET.a[an]i.db .
+      N=0
+   fi
+}
+
+# Find 95%ANI clade(s) with AAI <= 90% / ANI <= 95%
+REFGENOMES=$(cat ../10.clades/01.find/miga-project.ani95-clades \
+   | tail -n +2 | cut -d , -f 1)
+for i in $REFGENOMES ; do
+   AAI=$(aai.rb -1 ../06.cds/$DATASET.faa \
+      -2 ../06.cds/$i.faa -t $CORES -a --lookup-first \
+      -S $TMPDIR/$DATASET.aai.db --name1 $DATASET --name2 $i || echo "")
+   checkpoint_n
+   if [[ $(perl -MPOSIX -e "print ceil $AAI") -ge 90 ]] ; then
+      echo $i >> $DATASET.aai90
+      [[ -e "../05.assembly/$DATASET.LargeContigs.fna" ]] || continue
+      [[ -e "../05.assembly/$i.LargeContigs.fna" ]] || continue
+      ANI=$(ani.rb -1 ../05.assembly/$DATASET.LargeContigs.fna \
+	 -2 ../05.assembly/$i.LargeContigs.fna -t $CORES -a --lookup-first \
+	 -S $TMPDIR/$DATASET.ani.db --name1 $DATASET --name2 $i || echo "")
+      checkpoint_n
+      if [[ $(perl -MPOSIX -e "print ceil $ANI") -ge 95 ]] ; then
+	 echo $i >> $DATASET.ani95
+      fi
+   fi
+done
+
+# Classify in-clade (if project type is clade)
+CLADES="../10.clades/02.ani"
+CLASSIF="."
+MAX_ANI=0
+ANI_MED=""
+[[ -e "$DATASET.medoids" ]] && rm "$DATASET.medoids"
+while [[ -e "$CLADES/$CLASSIF/miga-project.1.medoids" ]] ; do
+   for i in $(cat "$CLADES/$CLASSIF/miga-project.1.medoids") ; do
+      ANI=$(ani.rb -1 ../05.assembly/$DATASET.LargeContigs.fna \
+	 -2 ../05.assembly/$i.LargeContigs.fna -t $CORES -a --lookup-first \
+	 -S $TMPDIR/$DATASET.ani.db --name1 $DATASET --name2 $i || echo "")
+      checkpoint_n
+      if [[ $(perl -e "print 1 if $ANI > $MAX_ANI") == "1" ]] ; then
+         MAX_ANI=$ANI
+	 ANI_MED=$i
+      fi
+   done
+   echo $i >> "$DATASET.medoids"
+   CLASSIF="$CLASSIF/miga-project.1.subcl-$i"
+done
+echo $CLASSIF > "$DATASET.class"
+
+# Calculate all the ANIs against the lowest subclade (if classified in-clade)
+if [[ "$CLASSIF" != "." ]] ; then
+   for i in $(cat "$CLADES/$CLASSIF/miga-project.all") ; do
+      ANI=$(ani.rb -1 ../05.assembly/$DATASET.LargeContigs.fna \
+	 -2 ../05.assembly/$i.LargeContigs.fna -t $CORES -a --lookup-first \
+	 -S $TMPDIR/$DATASET.ani.db --name1 $DATASET --name2 $i || echo "")
+      checkpoint_n
+   done
+fi
+
+# Finalize
+mv $TMPDIR/$DATASET.a[an]i.db .
+
--- scripts/distances.bash
+++ scripts/distances.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Available variables: $PROJECT, $RUNTYPE, $MIGA, $CORES
+# Available variables: $PROJECT, $DATASET, $RUNTYPE, $MIGA, $CORES
 echo "MiGA: $MIGA"
 echo "Project: $PROJECT"
 source "$MIGA/scripts/miga.bash" || exit 1
@@ -9,102 +9,21 @@ cd "$PROJECT/data/09.distances"
 date "+%Y-%m-%d %H:%M:%S %z" > "$DATASET.start"
 TMPDIR=$(mktemp -d /tmp/MiGA.XXXXXXXXXXXX)
 
-function checkpoint_n {
-   if [[ $N -eq 10 ]] ; then
-      for t in 01.haai 02.aai 03.ani ; do
-         [[ -s $TMPDIR/$t.db ]] && cp $TMPDIR/$t.db $t/$DATASET.db
-      done
-      N=0
-   fi
-   let N=$N+1
-}
-
 # Check type of dataset
 NOMULTI=$(miga list_datasets -P "$PROJECT" -D "$DATASET" --no-multi \
    | wc -l | awk '{print $1}')
-ESS="../07.annotation/01.function/01.essential"
-if [[ "$NOMULTI" -eq "1" ]] ; then
-   # Initialize temporals
-   for t in 01.haai 02.aai 03.ani ; do
-      [[ -s $t/$DATASET.db ]] && cp $t/$DATASET.db $TMPDIR/$t.db
-   done
-   echo "create table if not exists aai(seq1 varchar(256), seq2 varchar(256)," \
-      "aai float, sd float, n int, omega int);" | sqlite3 $TMPDIR/02.aai.db
-   N=1
-   # Traverse "nearly-half" of the ref-datasets using first-come-first-served
-   for i in $(miga list_datasets -P "$PROJECT" --ref --no-multi) ; do
-      echo "=[ $i ]"
-      date "+%Y-%m-%d %H:%M:%S %z"
-      # Check if the i-th dataset is ready
-      [[ -s $ESS/$i.done && -s $ESS/$i.json ]] || continue
-      # Check if this is done (e.g., in a previous failed iteration)
-      AAI=$(echo "select aai from aai where seq1='$DATASET' and seq2='$i';" \
-	 | sqlite3 $TMPDIR/02.aai.db || echo "")
-      # Try the other direction
-      if [[ "$AAI" == "" && -s 02.aai/$i.db ]] ; then
-	 cp "02.aai/$i.db" "$TMPDIR/$i.db"
-	 AAI=$(echo "select aai from aai where seq2='$DATASET' and seq1='$i';" \
-	    | sqlite3 "$TMPDIR/$i.db" || echo "")
-	 rm "$TMPDIR/$i.db"
-      fi
-      # Try with hAAI
-      if [[ "$AAI" == "" ]] ; then
-	 [[ -e "$TMPDIR/$DATASET.ess.faa" ]] \
-	    || cp $ESS/$DATASET.ess.faa $TMPDIR/$DATASET.ess.faa
-	 HAAI=$(aai.rb -1 $TMPDIR/$DATASET.ess.faa -2 $ESS/$i.ess.faa \
-	    -t $CORES -a -n 10 -S $TMPDIR/01.haai.db --name1 $DATASET \
-	    --name2 $i || echo "")
-	 if [[ "$HAAI" != "" \
-	       && $(perl -MPOSIX -e "print floor $HAAI") -lt 90 ]] ; then
-	    AAI=$(perl -e \
-	       "printf '%f', 100-exp(2.435076 + 0.4275193*log(100-$HAAI))")
-	    echo "insert into aai values('$DATASET','$i','$AAI',0,0,0);" \
-	       | sqlite3 $TMPDIR/02.aai.db
-	 fi
-      fi
-      # Try with complete AAI
-      if [[ "$AAI" == "" ]] ; then
-	 [[ -e "$TMPDIR/$DATASET.faa" ]] \
-	    || cp ../06.cds/$DATASET.faa $TMPDIR/$DATASET.faa
-	 AAI=$(aai.rb -1 $TMPDIR/$DATASET.faa -2 ../06.cds/$i.faa -t $CORES -a \
-	    -S $TMPDIR/02.aai.db --name1 $DATASET --name2 $i || echo "")
-      fi
-      date "+%Y-%m-%d %H:%M:%S %z"
-      # Check if ANI is meaningful
-      if [[ -e "../05.assembly/$DATASET.LargeContigs.fna" \
-	    && -e "../05.assembly/$i.LargeContigs.fna" \
-	    && $(perl -MPOSIX -e "print ceil $AAI") -gt 90 ]] ; then
-	 # Check if this is done (e.g., in a previous failed iteration)
-	 ANI=$(echo "select ani from ani where seq1='$DATASET' and seq2='$i';" \
-	    | sqlite3 $TMPDIR/03.ani.db || echo "")
-	 # Try the other direction
-	 if [[ "$ANI" == "" && -s 03.ani/$i.db ]] ; then
-	    cp "03.ani/$i.db" "$TMPDIR/$i.db"
-	    ANI=$(echo "select ani from ani" \
-	       "where seq2='$DATASET' and seq1='$i';" \
-	       | sqlite3 "$TMPDIR/$i.db" || echo "")
-	    rm "$TMPDIR/$i.db"
-	 fi
-	 # Calculate it
-	 if [[ "$ANI" == "" ]] ; then
-	    [[ -e "$TMPDIR/$DATASET.LargeContigs.fna" ]] \
-	       || cp ../05.assembly/$DATASET.LargeContigs.fna \
-		  $TMPDIR/$DATASET.LargeContigs.fna
-	    ANI=$(ani.rb -1 $TMPDIR/$DATASET.LargeContigs.fna \
-	       -2 ../05.assembly/$i.LargeContigs.fna -t $CORES \
-	       -S $TMPDIR/03.ani.db -a --name1 $DATASET --name2 $i || echo "")
-	 fi
-      fi
-      echo "$AAI;$ANI"
-      checkpoint_n
-   done
-   N=10
-   checkpoint_n
+REF=$(miga list_datasets -P "$PROJECT" -D "$DATASET" --ref \
+   | wc -l | awk '{print $1}')
+
+# Call submodule
+if [[ "$NOMULTI" -eq "1" && "$REF" -eq "1" ]] ; then
+   source "$MIGA/scripts/_distances_ref_nomulti.bash"
+elif [[ "$NOMULTI" -eq "1" ]] ; then
+   source "$MIGA/scripts/_distances_noref_nomulti.bash"
 fi
 
-rm -R $TMPDIR
-
 # Finalize
+rm -R $TMPDIR
 date "+%Y-%m-%d %H:%M:%S %z" > "$DATASET.done"
 miga add_result -P "$PROJECT" -D "$DATASET" -r distances
 
