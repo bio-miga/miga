f36cb9bb8c679d6946f7239dcd7b77fb1bd4da2f
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-04-06 23:05:20 -0400

    Cleanup of MiGA::RemoteDataset

--- lib/miga/remote_dataset.rb
+++ lib/miga/remote_dataset.rb
@@ -1,154 +1,193 @@
-#
 # @package MiGA
-# @author Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
-# @license artistic license 2.0
-# @update Dec-07-2015
-#
+# @license Artistic-2.0
 
 require "restclient"
 
-module MiGA
-   class RemoteDataset
-      # Class
-      @@UNIVERSE = {
-	 ebi:{
-	    dbs: { embl:{stage: :assembly, format: :fasta} },
-	    url: "http://www.ebi.ac.uk/Tools/dbfetch/dbfetch/%1$s/%2$s/%3$s",
-	    method: :rest
-	 },
-	 ncbi:{
-	    dbs: { nuccore:{stage: :assembly, format: :fasta} },
-	    url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/" +
-	       "efetch.fcgi?db=%1$s&id=%2$s&rettype=%3$s&retmode=text",
-	    method: :rest
-	 },
-	 ncbi_map:{
-	    dbs: { assembly:{map_to: :nuccore, format: :text} },
-	    url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/" +
-	       "elink.fcgi?dbfrom=%1$s&id=%2$s&db=%3$s - - - - -",
-	    method: :rest,
-	    map_to_universe: :ncbi
-	 }
-      }
-      def self.UNIVERSE ; @@UNIVERSE ; end
-      def self.download(universe, db, ids, format, file=nil)
-	 ids = [ids] unless ids.is_a? Array
-	 case @@UNIVERSE[universe][:method]
-	 when :rest
-	    map_to = @@UNIVERSE[universe][:dbs][db].nil? ? nil :
-	       @@UNIVERSE[universe][:dbs][db][:map_to]
-	    url = sprintf @@UNIVERSE[universe][:url],
-	       db, ids.join(","), format, map_to
-	    response = RestClient::Request.execute(:method=>:get,  :url=>url,
-	       :timeout=>600)
-	    raise "Unable to reach #{universe} client, error code "+
-	       "#{response.code}." unless response.code == 200
-	    doc = response.to_s
-	 else
-	    raise "Unexpected error: Unsupported download method for Universe "+
-	       "#{universe}."
-	 end
-	 unless file.nil?
-	    ofh = File.open(file, "w")
-	    ofh.print doc
-	    ofh.close
-	 end
-	 doc
-      end
-      # Instance
-      attr_reader :universe, :db, :ids
-      def initialize(ids, db, universe)
-	 ids = [ids] unless ids.is_a? Array
-	 @ids = (ids.is_a?(Array) ? ids : [ids])
-	 @db = db.to_sym
-	 @universe = universe.to_sym
-	 raise "Unknown Universe: #{@universe}. Try one of: "+
-	    "#{@@UNIVERSE.keys}" unless @@UNIVERSE.keys.include? @universe
-	 raise "Unknown Database: #{@db}. Try one of: "+
-	    "#{@@UNIVERSE[@universe][:dbs]}" unless
-	    @@UNIVERSE[@universe][:dbs].include? @db
-	 unless @@UNIVERSE[@universe][:dbs][@db][:map_to].nil?
-	    res = RemoteDataset.download
-	 end
-      end
-      def save_to(project, name=nil, is_ref=true, metadata={})
-	 name = ids.join("_").miga_name if name.nil?
-	 project = Project.new(project) if project.is_a? String
-	 raise "Dataset #{name} exists in the project, aborting..." if
-	    Dataset.exist?(project, name)
-	 metadata = get_metadata(metadata)
-	 case @@UNIVERSE[universe][:dbs][db][:stage]
-	 when :assembly
-	    base = project.path + "/data/" + Dataset.RESULT_DIRS[:assembly] +
-	       "/" + name
-	    ofh = File.open("#{base}.start", "w")
-	    ofh.puts Time.now.to_s
-	    ofh.close
-	    download("#{base}.LargeContigs.fna")
-	    File.symlink("#{base}.LargeContigs.fna", "#{base}.AllContigs.fna")
-	    ofh = File.open("#{base}.done", "w")
-	    ofh.puts Time.now.to_s
-	    ofh.close
-	 else
-	    raise "Unexpected error: Unsupported result for database #{db}."
-	 end
-	 dataset = Dataset.new(project, name, is_ref, metadata)
-	 project.add_dataset(dataset.name)
-	 result = dataset.add_result @@UNIVERSE[universe][:dbs][db][:stage]
-	 raise "Empty dataset created: seed result was not added due to "+
-	    "incomplete files." if result.nil?
-	 dataset
-      end
-      def get_metadata(metadata={})
-	 case universe
-	 when :ebi
-	    # Get taxonomy
-	    metadata[:tax] = get_ncbi_taxonomy
-	 when :ncbi
-	    # Get taxonomy
-	    metadata[:tax] = get_ncbi_taxonomy
-	 end
-	 metadata
-      end
-      def download(file)
-	 RemoteDataset.download(universe, db, ids,
-	    @@UNIVERSE[universe][:dbs][db][:format], file)
-      end
-      def get_ncbi_taxid
-	 case universe
-	 when :ebi
-	    doc = RemoteDataset.download(universe, db, ids, :annot).split(/\n/)
-	    ln = doc.grep(/^FT\s+\/db_xref="taxon:/).first
-	    ln = doc.grep(/^OX\s+NCBI_TaxID=/).first if ln.nil?
-	    return nil if ln.nil?
-	    ln.sub!(/.*(?:"taxon:|NCBI_TaxID=)(\d+)["; ].*/, "\\1")
-	    return nil unless ln =~ /^\d+$/
-	    ln
-	 when :ncbi
-	    doc = RemoteDataset.download(universe, db, ids, :gb).split(/\n/)
-	    ln = doc.grep(/^\s+\/db_xref="taxon:/).first
-	    return nil if ln.nil?
-	    ln.sub!(/.*(?:"taxon:)(\d+)["; ].*/, "\\1")
-	    return nil unless ln =~ /^\d+$/
-	    ln
-	 else
-	    raise "I don't know how to extract ncbi_taxids from #{universe}."
-	 end
-      end
-      def get_ncbi_taxonomy
-	 lineage = {}
-	 tax_id = get_ncbi_taxid
-	 loop do
-	    break if tax_id.nil? or %w{0 1}.include? tax_id
-	    doc = RemoteDataset.download(:ebi, :taxonomy, tax_id, "")
-	    name = (doc.scan(/SCIENTIFIC NAME\s+:\s+(.+)/).first||[]).first
-	    rank = (doc.scan(/RANK\s+:\s+(.+)/).first||[]).first
-	    rank = "dataset" if lineage.empty? and rank=="no rank"
-	    lineage[rank] = name unless rank.nil?
-	    tax_id = (doc.scan(/PARENT ID\s+:\s+(.+)/).first||[]).first
-	 end
-	 Taxonomy.new(lineage)
-      end
-   end
-end
+##
+# MiGA representation of datasets with data in remote locations.
+class MiGA::RemoteDataset < MiGA::MiGA
+  # Class-level
 
+  ##
+  # Structure of the different database Universes or containers. The structure
+  # is a Hash with universe names as keys as Symbol and values being a Hash with
+  # supported keys as Symbol:
+  # - +:dbs+ => Hash with keys being the database name and the values a Hash of
+  #   properties such as +stage+, +format+, and +map_to+.
+  # - +url+ => Pattern of the URL where the data can be obtained, where +%1$s+
+  #   is the name of the database, +%2$s+ is the IDs, and +%3$s+ is format.
+  # - +method+ => Method used to query the URL. Only +:rest+ is currently
+  #   supported.
+  # - +map_to_universe+ => Universe where results map to. Currently unsupported.
+  def self.UNIVERSE ; @@UNIVERSE ; end
+  @@UNIVERSE = {
+    ebi:{
+      dbs: { embl:{stage: :assembly, format: :fasta} },
+      url: "http://www.ebi.ac.uk/Tools/dbfetch/dbfetch/%1$s/%2$s/%3$s",
+      method: :rest
+    },
+    ncbi:{
+      dbs: { nuccore:{stage: :assembly, format: :fasta} },
+      url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/" +
+	"efetch.fcgi?db=%1$s&id=%2$s&rettype=%3$s&retmode=text",
+      method: :rest
+    },
+    ncbi_map:{
+      dbs: { assembly:{map_to: :nuccore, format: :text} },
+      url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/" +
+	# FIXME ncbi_map is intended to do internal NCBI mapping between
+	# databases.
+	"elink.fcgi?dbfrom=%1$s&id=%2$s&db=%3$s - - - - -",
+      method: :rest,
+      map_to_universe: :ncbi
+    }
+  }
+
+  ##
+  # Download data from the +universe+ in the database +db+ with IDs +ids+ and
+  # in +format+. If passed, it saves the result in +file+. Returns String.
+  def self.download(universe, db, ids, format, file=nil)
+    ids = [ids] unless ids.is_a? Array
+    case @@UNIVERSE[universe][:method]
+    when :rest
+      map_to = @@UNIVERSE[universe][:dbs][db].nil? ? nil :
+	@@UNIVERSE[universe][:dbs][db][:map_to]
+      url = sprintf @@UNIVERSE[universe][:url],
+	db, ids.join(","), format, map_to
+      response = RestClient::Request.execute(:method=>:get,  :url=>url,
+	:timeout=>600)
+      raise "Unable to reach #{universe} client, error code "+
+	"#{response.code}." unless response.code == 200
+      doc = response.to_s
+    else
+      raise "Unexpected error: Unsupported download method for Universe "+
+	"#{universe}."
+    end
+    unless file.nil?
+      ofh = File.open(file, "w")
+      ofh.print doc
+      ofh.close
+    end
+    doc
+  end
+
+  # Instance-level
+
+  ##
+  # Universe of the dataset.
+  attr_reader :universe
+  # Database storing the dataset.
+  attr_reader :db
+  # IDs of the entries composing the dataset.
+  attr_reader :ids
+
+  ##
+  # Initialize MiGA::RemoteDataset with +ids+ in database +db+ from +universe+.
+  def initialize(ids, db, universe)
+    ids = [ids] unless ids.is_a? Array
+    @ids = (ids.is_a?(Array) ? ids : [ids])
+    @db = db.to_sym
+    @universe = universe.to_sym
+    raise "Unknown Universe: #{@universe}. Try one of: "+
+      "#{@@UNIVERSE.keys}" unless @@UNIVERSE.keys.include? @universe
+    raise "Unknown Database: #{@db}. Try one of: "+
+      "#{@@UNIVERSE[@universe][:dbs]}" unless
+      @@UNIVERSE[@universe][:dbs].include? @db
+    # FIXME Part of the +map_to+ support:
+    #unless @@UNIVERSE[@universe][:dbs][@db][:map_to].nil?
+    #  MiGA::RemoteDataset.download
+    #end
+  end
+
+  ##
+  # Save dataset to the MiGA::Project +project+ identified with +name+. +is_ref+
+  # indicates if it should be a reference dataset, and contains +metadata+.
+  def save_to(project, name=nil, is_ref=true, metadata={})
+    name = ids.join("_").miga_name if name.nil?
+    project = MiGA::Project.new(project) if project.is_a? String
+    raise "Dataset #{name} exists in the project, aborting..." if
+      MiGA::Dataset.exist?(project, name)
+    metadata = get_metadata(metadata)
+    case @@UNIVERSE[universe][:dbs][db][:stage]
+    when :assembly
+      base = project.path + "/data/" + MiGA::Dataset.RESULT_DIRS[:assembly] +
+	"/" + name
+      File.open("#{base}.start", "w") { |ofh| ofh.puts Time.now.to_s }
+      download("#{base}.LargeContigs.fna")
+      File.symlink("#{base}.LargeContigs.fna", "#{base}.AllContigs.fna")
+      File.open("#{base}.done", "w") { |ofh| ofh.puts Time.now.to_s }
+    else
+      raise "Unexpected error: Unsupported result for database #{db}."
+    end
+    dataset = MiGA::Dataset.new(project, name, is_ref, metadata)
+    project.add_dataset(dataset.name)
+    result = dataset.add_result @@UNIVERSE[universe][:dbs][db][:stage]
+    raise "Empty dataset created: seed result was not added due to "+
+      "incomplete files." if result.nil?
+    dataset
+  end
+
+  ##
+  # Get metadata from the remote location.
+  def get_metadata(metadata={})
+    case universe
+    when :ebi
+      # Get taxonomy
+      metadata[:tax] = get_ncbi_taxonomy
+    when :ncbi
+      # Get taxonomy
+      metadata[:tax] = get_ncbi_taxonomy
+    end
+    metadata
+  end
+
+  ##
+  # Download data into +file+.
+  def download(file)
+    MiGA::RemoteDataset.download(universe, db, ids,
+      @@UNIVERSE[universe][:dbs][db][:format], file)
+  end
+
+  ##
+  # Get NCBI Taxonomy ID.
+  def get_ncbi_taxid
+    send("get_ncbi_taxid_from_#{universe}")
+  end
+
+  ##
+  # Get NCBI taxonomy as MiGA::Taxonomy.
+  def get_ncbi_taxonomy
+    lineage = {}
+    tax_id = get_ncbi_taxid
+    while !(tax_id.nil? or %w{0 1}.include? tax_id)
+      doc = MiGA::RemoteDataset.download(:ebi, :taxonomy, tax_id, "")
+      name = doc.scan(/SCIENTIFIC NAME\s+:\s+(.+)/).first.to_a.first
+      rank = doc.scan(/RANK\s+:\s+(.+)/).first.to_a.first
+      rank = "dataset" if lineage.empty? and rank=="no rank"
+      lineage[rank] = name unless rank.nil?
+      tax_id = doc.scan(/PARENT ID\s+:\s+(.+)/).first.to_a.first
+    end
+    MiGA::Taxonomy.new(lineage)
+  end
+
+  private
+    
+    def get_ncbi_taxid_from_ncbi
+      doc = MiGA::RemoteDataset.download(universe, db, ids, :gb).split(/\n/)
+      ln = doc.grep(/^\s+\/db_xref="taxon:/).first
+      return nil if ln.nil?
+      ln.sub!(/.*(?:"taxon:)(\d+)["; ].*/, "\\1")
+      return nil unless ln =~ /^\d+$/
+      ln
+    end
+
+    def get_ncbi_taxid_from_ebi
+      doc = MiGA::RemoteDataset.download(universe, db, ids, :annot).split(/\n/)
+      ln = doc.grep(/^FT\s+\/db_xref="taxon:/).first
+      ln = doc.grep(/^OX\s+NCBI_TaxID=/).first if ln.nil?
+      return nil if ln.nil?
+      ln.sub!(/.*(?:"taxon:|NCBI_TaxID=)(\d+)["; ].*/, "\\1")
+      return nil unless ln =~ /^\d+$/
+      ln
+    end
+end
---
+++ test/remote_dataset_test.rb
@@ -0,0 +1,41 @@
+require "test_helper"
+require "miga/project"
+require "miga/remote_dataset"
+
+class RemoteDatasetTest < Test::Unit::TestCase
+  
+  def setup
+    $tmp = Dir.mktmpdir
+    ENV["MIGA_HOME"] = $tmp
+    FileUtils.touch("#{ENV["MIGA_HOME"]}/.miga_rc")
+    FileUtils.touch("#{ENV["MIGA_HOME"]}/.miga_daemon.json")
+    $p1 = MiGA::Project.new(File.expand_path("project1", $tmp))
+    $remote_tests = ! ENV["REMOTE_TESTS"].nil?
+  end
+
+  def teardown
+    FileUtils.rm_rf $tmp
+    ENV["MIGA_HOME"] = nil
+  end
+  
+  def test_class_universe
+    assert_respond_to(MiGA::RemoteDataset, :UNIVERSE)
+    assert(MiGA::RemoteDataset.UNIVERSE.keys.include? :ebi)
+  end
+
+  def test_remote_dataset
+    assert_raise { MiGA::RemoteDataset.new("ids", :embl, :marvel) }
+    assert_raise { MiGA::RemoteDataset.new("ids", :google, :ebi) }
+  end
+
+  def test_ebi
+    hiv2 = "M30502.1"
+    rd = MiGA::RemoteDataset.new(hiv2, :embl, :ebi)
+    assert_equal([hiv2], rd.ids)
+    omit_if(!$remote_tests, "Remote access is error-prone.")
+    tx = rd.get_ncbi_taxonomy
+    assert_equal(MiGA::Taxonomy, tx.class)
+    assert_equal("Lentivirus", tx[:g])
+  end
+
+end
