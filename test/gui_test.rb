2a149d9fcde54eeb9e4359dcf59823f2513ded4d
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-04-12 16:36:35 -0400

    GUI tiny tests

--- test/gui_test.rb
+++ test/gui_test.rb
@@ -4,12 +4,17 @@ class GUITest < Test::Unit::TestCase
   
   def setup
     $gui_tests = !ENV["GUI_TESTS"].nil?
-    require "miga/gui" if $gui_tests
+    if $gui_tests
+      require "miga/gui"
+      $app = MiGA::GUI.init
+    end
   end
 
   def test_status
     omit_if(!$gui_tests, "GUI tested only in JRuby.")
     assert_respond_to(MiGA::GUI, :status)
+    assert_equal("MiGA is ready to go!", MiGA::GUI.status)
+    assert_equal(MiGA::GUI, $app.class)
   end
 
 end
