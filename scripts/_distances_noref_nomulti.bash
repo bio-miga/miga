2de95436aa2e201a6df4b616472de67587f7d865
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-04-27 12:00:55 -0400

    Fixed bug in distances for query datasets

--- scripts/_distances_noref_nomulti.bash
+++ scripts/_distances_noref_nomulti.bash
@@ -29,9 +29,12 @@ if [[ $(miga project_info -P "$PROJECT" -m type) != "clade" ]] ; then
   CLASSIF="."
   MAX_AAI=0
   AAI_MED=""
+  AAI_CLS=""
   [[ -e "$DATASET.aai-medoids.tsv" ]] && rm "$DATASET.aai-medoids.tsv"
   while [[ -e "$CLADES/$CLASSIF/miga-project.1.medoids" ]] ; do
+    i_n=0
     for i in $(cat "$CLADES/$CLASSIF/miga-project.1.medoids") ; do
+      let i_n=$i_n+1
       AAI=$(aai.rb -1 ../06.cds/$DATASET.faa \
         -2 ../06.cds/$i.faa -t $CORES -a --lookup-first \
         -S $TMPDIR/$DATASET.aai.db --name1 $DATASET --name2 $i || echo "")
@@ -39,10 +42,12 @@ if [[ $(miga project_info -P "$PROJECT" -m type) != "clade" ]] ; then
       if [[ $(perl -e "print 1 if $AAI >= $MAX_AAI") == "1" ]] ; then
         MAX_AAI=$AAI
         AAI_MED=$i
+        AAI_CLS=$i_n
       fi
     done
-    CLASSIF="$CLASSIF/miga-project.1.subcl-$AAI_MED"
-    echo "$AAI_MED	$MAX_AAI	$CLASSIF" >> "$DATASET.aai-medoids.tsv"
+    CLASSIF="$CLASSIF/miga-project.1.subcl-$AAI_CLS"
+    echo "$AAI_CLS      $AAI_MED	$MAX_AAI	$CLASSIF" \
+      >> "$DATASET.aai-medoids.tsv"
   done
 else
   # Classify ani-clade (if project type is clade)
@@ -50,9 +55,12 @@ else
   CLASSIF="."
   MAX_ANI=0
   ANI_MED=""
+  ANI_CLS=""
   [[ -e "$DATASET.ani-medoids.tsv" ]] && rm "$DATASET.ani-medoids.tsv"
   while [[ -e "$CLADES/$CLASSIF/miga-project.1.medoids" ]] ; do
+    i_n=0
     for i in $(cat "$CLADES/$CLASSIF/miga-project.1.medoids") ; do
+      let i_n=$i_n+1
       ANI=$(ani.rb -1 ../05.assembly/$DATASET.LargeContigs.fna \
         -2 ../05.assembly/$i.LargeContigs.fna -t $CORES -a \
         --no-save-regions --no-save-rbm --lookup-first \
@@ -61,10 +69,12 @@ else
       if [[ $(perl -e "print 1 if $ANI >= $MAX_ANI") == "1" ]] ; then
         MAX_ANI=$ANI
         ANI_MED=$i
+        AAI_CLS=$i_n
       fi
     done
-    CLASSIF="$CLASSIF/miga-project.1.subcl-$ANI_MED"
-    echo "$ANI_MED	$MAX_ANI	$CLASSIF" >> "$DATASET.ani-medoids.tsv"
+    CLASSIF="$CLASSIF/miga-project.1.subcl-$ANI_CLS"
+    echo "$ANI_CLS      $ANI_MED	$MAX_ANI	$CLASSIF" \
+      >> "$DATASET.ani-medoids.tsv"
   done
 
   # Calculate all the ANIs against the lowest subclade (if classified in-clade)
@@ -80,3 +90,7 @@ else
     fi
   fi
 fi
+
+#Finalize
+N=11
+checkpoint_n
