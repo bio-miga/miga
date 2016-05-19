8aff4ecca34e86dd7351fc17100efe96eb5d257b
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-05-19 17:10:52 -0400

    Now pulling enveomics.R from CRAN

--- scripts/init.bash
+++ scripts/init.bash
@@ -106,11 +106,7 @@ echo "export PATH=\$MIGA_PATH\$PATH" >> "$HOME/.miga_rc"
 # Check for R packages
 echo "
 Looking for R packages:" >&2
-if ! check_rlib enveomics.R ; then
-   echo "+ Installing enveomics.R" >&2
-   R CMD INSTALL $(dirname "$(which "FastQ.tag.rb")")/../enveomics.R
-fi
-RLIBS="ape ggdendro ggplot2 gridExtra cluster dendextend vegan scatterplot3d"
+RLIBS="enveomics.R ape ggdendro ggplot2 gridExtra cluster dendextend vegan scatterplot3d"
 for lib in $RLIBS ; do
    if ! check_rlib $lib ; then
       echo "+ Installing $lib" >&2
