88e12def85802093876cbb01917a88672ca06bd2
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-05-19 18:24:49 -0400

    Reduced I/O accesses for dataset result traversals

--- lib/miga/dataset.rb
+++ lib/miga/dataset.rb
@@ -179,7 +179,8 @@ class MiGA::Dataset < MiGA::MiGA
     return nil if @@RESULT_DIRS[result_type].nil?
     base = project.path + "/data/" + @@RESULT_DIRS[result_type] +
       "/" + name
-    return MiGA::Result.load(base + ".json") unless save
+    return MiGA::Result.load("#{base}.json") if
+      !save or File.size? "#{base}.json"
     return nil unless result_files_exist?(base, ".done")
     r = self.send("add_result_#{result_type}", base)
     r.save unless r.nil?
