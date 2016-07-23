abb4a0e19854766ac9a2f90467fb9e37a21179d3
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-07-23 16:37:03 -0400

    Read access to dataset metadata from the CLI

--- actions/create_dataset.rb
+++ actions/create_dataset.rb
@@ -16,6 +16,10 @@ OptionParser.new do |opt|
     "Owner of the dataset."){ |v| o[:user]=v }
   opt.on("-c", "--comments STRING",
     "Comments on the dataset."){ |v| o[:comments]=v }
+  opt.on("-m", "--metadata STRING",
+    "Metadata as key-value pairs separated by = and delimited by comma.",
+    "Values are saved as strings except for booleans (true / false) or nil."
+    ){ |v| o[:metadata]=v }
   opt.on("--update",
     "Updates the dataset if it already exists."){ o[:update]=true }
   opt_common(opt, o)
@@ -34,6 +38,20 @@ $stderr.puts "Loading dataset." unless o[:q]
 d = o[:update] ? p.dataset(o[:dataset]) :
   MiGA::Dataset.new(p, o[:dataset], o[:ref], {})
 raise "Dataset does not exist." if d.nil?
+unless o[:metadata].nil?
+  o[:metadata].split(",") do |pair|
+    (k,v) = pair.split("=")
+    case v
+    when "true"
+      v = true
+    when "false"
+      v = false
+    when "nil"
+      v = nil
+    end
+    d.metadata[k] = v
+  end
+end
 [:type, :description, :user, :comments].each do |k|
   d.metadata[k]=o[k] unless o[k].nil?
 end
