854f5d6390c041f85302054fa99c1a1b0a90e8f2
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-11-03 12:29:30 -0400

    Bypassing essential_genes if cds .faa is empty

--- scripts/essential_genes.bash
+++ scripts/essential_genes.bash
@@ -8,6 +8,16 @@ cd "$PROJECT/data/07.annotation/01.function/01.essential"
 
 # Initialize
 date "+%Y-%m-%d %H:%M:%S %z" > "$DATASET.start"
+FAA="../../../06.cds/$DATASET.faa"
+
+# Check if there are any proteins
+if [[ ! -s $FAA ]] ; then
+  echo Empty protein set, bypassing essential genes
+  rm "$DATASET.start"
+  miga create_dataset -P "$PROJECT" -D $DATASET \
+    -m run_essential_genes=false --update
+  exit 0
+fi
 
 # Find and extract essential genes
 [[ -d "$DATASET.ess" ]] && rm -R "$DATASET.ess"
@@ -15,11 +25,11 @@ mkdir "$DATASET.ess"
 TYPE=$(miga list_datasets -P "$PROJECT" -D "$DATASET" \
    --metadata "type" | awk '{print $2}')
 if [[ "$TYPE" == "metagenome" || "$TYPE" == "virome" ]] ; then
-   HMM.essential.rb -i "../../../06.cds/$DATASET.faa" -o "$DATASET.ess.faa" \
+   HMM.essential.rb -i "$FAA" -o "$DATASET.ess.faa" \
       -m "$DATASET.ess/" -t "$CORES" -r "$DATASET" --metagenome \
       > "$DATASET.ess/log"
 else
-   HMM.essential.rb -i "../../../06.cds/$DATASET.faa" -o "$DATASET.ess.faa" \
+   HMM.essential.rb -i "$FAA" -o "$DATASET.ess.faa" \
       -m "$DATASET.ess/" -t "$CORES" -r "$DATASET" \
       > "$DATASET.ess/log"
 fi
