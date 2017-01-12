cc6d625be6b8d2fba038b3d11bafbb5ebc05d6af
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2017-01-12 15:01:14 -0500

    Recent typo in subclades definition fixed

--- utils/subclades.R
+++ utils/subclades.R
@@ -47,8 +47,8 @@ subclades <- function(ani_file, out_base, thr=1, ani=c()) {
   stopCluster(cl)
   s.avg.z <- (s[1,]-mean(s[1,]))/sd(s[1,])
   s.neg.z <- (s[2,]-mean(s[2,]))/sd(s[2,])
-  ds <- s.avg.z - s.neg.z + 2/(1:nrow(s))
-  top.n <- k[which.min(ds)]
+  ds <- s.avg.z - s.neg.z - 2/(1:nrow(s))
+  top.n <- k[which.max(ds)]
   
   # Classify genomes
   say("Classify")
