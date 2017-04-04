446b304f4fcc2e68730dc34358afd662f636f418
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2017-04-04 00:09:43 -0400

    MetaGeneMark -> Prodigal

--- scripts/cds.bash
+++ scripts/cds.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Available variables: $PROJECT, $RUNTYPE, $MIGA, $CORES
+# Available variables: $PROJECT, $RUNTYPE, $MIGA, $CORES, $DATASET
 set -e
 SCRIPT="cds"
 echo "MiGA: $MIGA"
@@ -9,37 +9,19 @@ cd "$PROJECT/data/06.cds"
 
 # Initialize
 date "+%Y-%m-%d %H:%M:%S %z" > "$DATASET.start"
-GM=$(dirname -- $(which gmhmmp))
 
-# Register key
-if [[ ! -e .gm_key ]] ; then
-   if [[ -e "$GM/gm_key" ]] ; then
-      cp "$GM/gm_key" ".gm_key"
-   elif [[ -e "$GM/gm_key_64" ]] ; then
-      cp "$GM/gm_key_64" ".gm_key"
-   elif [[ -e "$GM/gm_key_32" ]] ; then
-      cp "$GM/gm_key_32" ".gm_key"
-   elif [[ -e "$GM/.gm_key" ]] ; then
-      cp "$GM/.gm_key" ".gm_key"
-   elif [[ -e "$HOME/.gm_key" ]] ; then
-      cp "$HOME/.gm_key" .
-   else
-      echo "Impossible to find MetaGeneMark key, please register your copy" \
-	 "and place the key in '$GM/gm_key'." >&2
-      exit 1
-   fi
-fi
-
-# Run MetaGeneMark
-gmhmmp -a -d -m "$GM/MetaGeneMark_v1.mod" -f G -o "$DATASET.gff2" \
-   "../05.assembly/$DATASET.LargeContigs.fna"
-
-# Extract
-perl "$GM/aa_from_gff.pl" < "$DATASET.gff2" > "$DATASET.faa"
-perl "$GM/nt_from_gff.pl" < "$DATASET.gff2" > "$DATASET.fna"
+# Run Prodigal
+TYPE=$(miga list_datasets -P "$PROJECT" -D "$DATASET" \
+   --metadata "type" | awk '{print $2}')
+case "$TYPE" in
+  metagenome|virome) PROCEDURE=meta ;;
+  *) PROCEDURE=single ;;
+esac
+prodigal -d "$DATASET.fna" -f gff -o "$DATASET.gff3" -p $PROCEDURE -q \
+  -i "../05.assembly/$DATASET.LargeContigs.fna"
 
 # Gzip
-gzip -9 -f "$DATASET.gff2"
+gzip -9 -f "$DATASET.gff3"
 
 # Finalize
 date "+%Y-%m-%d %H:%M:%S %z" > "$DATASET.done"
