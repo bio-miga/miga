04c437751e759a406867cb19d5972d6f5183f2a6
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2017-07-03 00:17:56 -0400

    Minor efficiency improvement for the daemon

--- lib/miga/daemon.rb
+++ lib/miga/daemon.rb
@@ -280,9 +280,10 @@ class MiGA::Daemon < MiGA::MiGA
   # Terminates a daemon.
   def terminate
     say "Terminating daemon..."
+    report_status
     k = runopts(:kill)
     @jobs_running.each do |i|
-      spawn(k % i[:pid])
+      `#{k % i[:pid]}`
       puts "Terminating pid:#{i[:pid]} for #{i[:task_name]}"
     end
     f = File.expand_path("daemon/alive", project.path)
