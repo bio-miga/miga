7ab7ffb06557d60ede09e632ea7b30a35e0905b8
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2017-09-02 06:07:42 -0400

    Bash-compatibility with torque

--- scripts/taxonomy.bash
+++ scripts/taxonomy.bash
@@ -45,7 +45,7 @@ if [[ "$S_PROJ" != "?" ]] ; then
     TAX_PVALUE=$(miga about -P "$PROJECT" -m tax_pvalue)
     [[ "$TAX_PVALUE" == "?" ]] && TAX_PVALUE="0.05"
     NEW_TAX=$(tail -n +6 "$DATASET.intax.txt" | head -n -3 \
-      | awk '$3<'$TAX_PVALUE'{print $1":"$2}' | tr '\n' ' ' \
+      | awk '$3<'$TAX_PVALUE'{print $1":"$2}' | tr "\\n" ' ' \
       | perl -pe 's/ *$//')
     miga tax_set -P "$PROJECT" -D "$DATASET" -s "$NEW_TAX"
   fi
