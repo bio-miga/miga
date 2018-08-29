490e6d72bdb333aca151610e7cb9cfdba194a645
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2018-08-29 18:50:50 -0400

    Result supports now time of start with the embedded style

--- lib/miga/result/dates.rb
+++ lib/miga/result/dates.rb
@@ -1,24 +1,25 @@
 
-require "miga/result/base"
+require 'miga/result/base'
 
 ##
 # Helper module including date-specific functions for results.
 module MiGA::Result::Dates
-
   include MiGA::Result::Base
-  
+
   ##
-  # Returns the start date of processing as DateTime or +nil+ if it doesn't exist.
+  # Returns the start date of processing as DateTime or +nil+ if it doesn't
+  # exist.
   def started_at
     date_at :start
   end
 
   ##
-  # Returns the end (done) date of processing as DateTime or +nil+ if it doesn't exist.
+  # Returns the end (done) date of processing as DateTime or +nil+ if it doesn't
+  # exist.
   def done_at
     date_at :done
   end
-  
+
   ##
   # Time it took for the result to complete as Float in minutes.
   def running_time
@@ -27,16 +28,17 @@ module MiGA::Result::Dates
     (b - a).to_f * 24 * 60
   end
 
-
   private
     
     ##
     # Internal function to detect start and end dates
     def date_at(event)
-      f = path event
-      return nil unless File.size? f
-      DateTime.parse File.read(f)
+      date = self[event]
+      if date.nil?
+        f = path event
+        date = File.read(f) if File.size? f
+      end
+      date.nil? ? nil : DateTime.parse(date)
     end
-
 end
 
