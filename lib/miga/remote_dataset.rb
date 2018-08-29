aadd18788edca0cfd40d4cd219f08c18ed723db2
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2018-08-29 19:56:29 -0400

    Cleans up code for RemoteDataset
    Close #34

--- lib/miga/remote_dataset.rb
+++ lib/miga/remote_dataset.rb
@@ -25,36 +25,35 @@ class MiGA::RemoteDataset < MiGA::MiGA
     @ids = (ids.is_a?(Array) ? ids : [ids])
     @db = db.to_sym
     @universe = universe.to_sym
-    raise "Unknown Universe: #{@universe}. Try one of: "+
-      "#{@@UNIVERSE.keys}" unless @@UNIVERSE.keys.include? @universe
-    raise "Unknown Database: #{@db}. Try one of: "+
-      "#{@@UNIVERSE[@universe][:dbs]}" unless
-      @@UNIVERSE[@universe][:dbs].include? @db
-    # FIXME Part of the +map_to+ support:
-    #unless @@UNIVERSE[@universe][:dbs][@db][:map_to].nil?
-    #  MiGA::RemoteDataset.download
-    #end
+    @@UNIVERSE.keys.include?(@universe) or
+      raise "Unknown Universe: #{@universe}. Try: #{@@UNIVERSE.keys}"
+    @@UNIVERSE[@universe][:dbs].include?(@db) or
+      raise "Unknown Database: #{@db}. Try: #{@@UNIVERSE[@universe][:dbs]}"
+    # FIXME: Part of the +map_to+ support:
+    # unless @@UNIVERSE[@universe][:dbs][@db][:map_to].nil?
+    #   MiGA::RemoteDataset.download
+    # end
   end
 
   ##
   # Save dataset to the MiGA::Project +project+ identified with +name+. +is_ref+
   # indicates if it should be a reference dataset, and contains +metadata+.
-  def save_to(project, name=nil, is_ref=true, metadata={})
-    name ||= ids.join("_").miga_name
+  def save_to(project, name = nil, is_ref = true, metadata = {})
+    name ||= ids.join('_').miga_name
     project = MiGA::Project.new(project) if project.is_a? String
     MiGA::Dataset.exist?(project, name) and
-          raise "Dataset #{name} exists in the project, aborting..."
+      raise "Dataset #{name} exists in the project, aborting..."
     metadata = get_metadata(metadata)
     udb = @@UNIVERSE[universe][:dbs][db]
     metadata["#{universe}_#{db}"] = ids.join(",")
     respond_to?("save_#{udb[:stage]}_to", true) or
-          raise "Unexpected error: Unsupported stage #{udb[:stage]} for #{db}."
+      raise "Unexpected error: Unsupported stage #{udb[:stage]} for #{db}."
     send "save_#{udb[:stage]}_to", project, name, udb
     dataset = MiGA::Dataset.new(project, name, is_ref, metadata)
     project.add_dataset(dataset.name)
-    result = dataset.add_result(udb[:stage], true, is_clean:true)
+    result = dataset.add_result(udb[:stage], true, is_clean: true)
     result.nil? and
-          raise "Empty dataset: seed result not added due to incomplete files."
+      raise 'Empty dataset: seed result not added due to incomplete files.'
     result.clean!
     result.save
     dataset
@@ -82,11 +81,11 @@ class MiGA::RemoteDataset < MiGA::MiGA
   def get_ncbi_taxonomy
     lineage = {}
     tax_id = get_ncbi_taxid
-    while !(tax_id.nil? or %w{0 1}.include? tax_id)
-      doc = MiGA::RemoteDataset.download(:ebi, :taxonomy, tax_id, "")
+    until [nil, '0', '1'].include? tax_id
+      doc = MiGA::RemoteDataset.download(:ebi, :taxonomy, tax_id, '')
       name = doc.scan(/SCIENTIFIC NAME\s+:\s+(.+)/).first.to_a.first
       rank = doc.scan(/RANK\s+:\s+(.+)/).first.to_a.first
-      rank = "dataset" if lineage.empty? and rank=="no rank"
+      rank = 'dataset' if lineage.empty? and rank == 'no rank'
       lineage[rank] = name unless rank.nil?
       tax_id = doc.scan(/PARENT ID\s+:\s+(.+)/).first.to_a.first
     end
@@ -97,7 +96,7 @@ class MiGA::RemoteDataset < MiGA::MiGA
 
     def get_ncbi_taxid_from_ncbi
       doc = MiGA::RemoteDataset.download(universe, db, ids, :gb).split(/\n/)
-      ln = doc.grep(/^\s+\/db_xref="taxon:/).first
+      ln = doc.grep(%r{^\s+/db_xref="taxon:}).first
       return nil if ln.nil?
       ln.sub!(/.*(?:"taxon:)(\d+)["; ].*/, "\\1")
       return nil unless ln =~ /^\d+$/
