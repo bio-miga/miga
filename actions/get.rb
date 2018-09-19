eab90df37b873dab59ecb1b93c393999597722bd
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2018-09-19 12:30:28 -0400

    New option: miga get -m

--- actions/get.rb
+++ actions/get.rb
@@ -3,35 +3,39 @@
 # @package MiGA
 # @license Artistic-2.0
 
-require "miga/remote_dataset"
+require 'miga/remote_dataset'
 
-o = {q:true, query:false, universe: :ebi, db: :embl}
+o = {q: true, query: false, universe: :ebi, db: :embl}
 OptionParser.new do |opt|
   opt_banner(opt)
   opt_object(opt, o, [:project, :dataset, :dataset_type])
-  opt.on("-I", "--ids ID1,ID2,...",
-    "(Mandatory unless -F) IDs in the remote database separated by commas."
+  opt.on('-I', '--ids ID1,ID2,...',
+    '(Mandatory unless -F) IDs in the remote database separated by commas.'
     ){ |v| o[:ids]=v }
-  opt.on("-U", "--universe STRING",
+  opt.on('-U', '--universe STRING',
     "Universe where the remote database lives. By default: #{o[:universe]}."
     ){ |v| o[:universe]=v.to_sym }
-  opt.on("--db STRING",
+  opt.on('--db STRING',
     "Name of the remote database. By default: #{o[:db]}."
     ){ |v| o[:db]=v.to_sym }
-  opt.on("-F", "--file PATH",
-    "Tab-delimited file (with header) listing the datasets to download.",
-    "The long form of all the options are supported as header (without the --)",
-    "including dataset, ids, universe, and db. For query use true/false values."
+  opt.on('-F', '--file PATH',
+    'Tab-delimited file (with header) listing the datasets to download.',
+    'The long form of all the options are supported as header (without the --)',
+    'including dataset, ids, universe, and db. For query use true/false values.'
     ){ |v| o[:file] = v }
-  opt.on("-q", "--query",
-    "If set, the dataset is registered as a query, not a reference dataset."
+  opt.on('-q', '--query',
+    'If set, the dataset is registered as a query, not a reference dataset.'
     ){ |v| o[:query]=v }
-  opt.on("--ignore-dup",
-    "If set, ignores datasets that already exist."){ |v| o[:ignore_dup]=v }
-  opt.on("-d", "--description STRING",
-    "Description of the dataset."){ |v| o[:description]=v }
-  opt.on("-c", "--comments STRING",
-    "Comments on the dataset."){ |v| o[:comments]=v }
+  opt.on('--ignore-dup',
+    'If set, ignores datasets that already exist.'){ |v| o[:ignore_dup]=v }
+  opt.on('-d', '--description STRING',
+    'Description of the dataset.'){ |v| o[:description]=v }
+  opt.on('-c', '--comments STRING',
+    'Comments on the dataset.'){ |v| o[:comments]=v }
+  opt.on('-m', '--metadata STRING',
+    'Metadata as key-value pairs separated by = and delimited by comma.',
+    'Values are saved as strings except for booleans (true / false) or nil.'
+    ){ |v| o[:metadata]=v }
   opt_common(opt, o)
 end.parse!
 
@@ -40,7 +44,7 @@ end.parse!
 glob = [o]
 unless o[:file].nil?
   glob = []
-  fh = File.open(o[:file], "r")
+  fh = File.open(o[:file], 'r')
   h = nil
   fh.each do |ln|
     r = ln.chomp.split(/\t/)
@@ -49,7 +53,7 @@ unless o[:file].nil?
     else
        glob << o.dup
        h.each_index do |i|
-	 glob[glob.size-1][h[i].to_sym] = h[i]=="query" ? r[i]=="true" :
+	 glob[glob.size-1][h[i].to_sym] = h[i]=='query' ? r[i]=='true' :
 	   %w[type universe db].include?(h[i]) ? r[i].to_sym : r[i]
        end
     end
@@ -58,25 +62,23 @@ unless o[:file].nil?
 end
 
 glob.each do |o_i|
-  opt_require(o_i, project:"-P", dataset:"-D", ids:"-I")
+  opt_require(o_i, project: '-P', dataset: '-D', ids: '-I')
 
   $stderr.puts "Dataset: #{o_i[:dataset]}" unless o_i[:q]
-  $stderr.puts "Loading project." unless o_i[:q]
+  $stderr.puts 'Loading project.' unless o_i[:q]
   p = MiGA::Project.load(o_i[:project])
   raise "Impossible to load project: #{o_i[:project]}" if p.nil?
 
   next if o_i[:ignore_dup] and not p.dataset(o_i[:dataset]).nil?
 
-  $stderr.puts "Locating remote dataset." unless o_i[:q]
+  $stderr.puts 'Locating remote dataset.' unless o_i[:q]
   rd = MiGA::RemoteDataset.new(o_i[:ids], o_i[:db], o_i[:universe])
 
-  $stderr.puts "Creating dataset." unless o_i[:q]
-  md = {}
-  [:type, :description, :user, :comments].each do |k|
-    md[k]=o_i[k] unless o_i[k].nil?
-  end
-  rd.save_to(p, o_i[:dataset], !o_i[:query], md)
+  $stderr.puts 'Creating dataset.' unless o_i[:q]
+  dummy_d = MiGA::Dataset.new(p, o_i[:dataset])
+  dummy_d = add_metadata(o_i, dummy_d)
+  rd.save_to(p, o_i[:dataset], !o_i[:query], dummy_d.metadata)
   p.add_dataset(o_i[:dataset])
 
-  $stderr.puts "Done." unless o_i[:q]
+  $stderr.puts 'Done.' unless o_i[:q]
 end
