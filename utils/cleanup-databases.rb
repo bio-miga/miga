bio-miga/miga

commit 4e969e60f269713490751822e1c1265fcbfa12f7

More efficient multi-threading of cleanup-databases.rb

utils/cleanup-databases.rb
@@ -6,17 +6,18 @@ require 'miga'
 ARGV[1] or abort "Usage: #{$0} path/to/project threads"
 
 $stderr.puts "Cleaning databases..."
-ds_list = MiGA::Project.load(ARGV[0]).datasets.
-  select(&:is_ref?).select(&:is_active?)
-
+p = MiGA::Project.load(ARGV[0])
+ds_names = p.dataset_names
 thr = ARGV[1].to_i
 
 (0 .. thr-1).each do |t|
   fork do
     k = -1
-    ds_list.each do |i|
+    ds_names.each do |i|
       k = (k+1) % thr
       next unless k == t
+      d = p.dataset(i)
+      next unless d.is_ref? and d.is_active?
       i.cleanup_distances!
     end
   end

commit e58cdaafe815ca86fb872568569a9b130782873b

Solves recent typo
Just introduced in 4e969e60f269713490751822e1c1265fcbfa12f7

utils/cleanup-databases.rb
@@ -18,7 +18,7 @@ thr = ARGV[1].to_i
       next unless k == t
       d = p.dataset(i)
       next unless d.is_ref? and d.is_active?
-      i.cleanup_distances!
+      d.cleanup_distances!
     end
   end
 end