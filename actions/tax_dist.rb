f41295f0d5a4c4181b32d751ea61e37266dd36a4
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-01-31 12:19:37 -0500

    miga tax_dist new columns n and omega

--- actions/tax_dist.rb
+++ actions/tax_dist.rb
@@ -3,34 +3,34 @@
 # @package MiGA
 # @license Artistic-2.0
 
-require "miga/tax_index"
-require "zlib"
-require "tmpdir"
+require 'miga/tax_index'
+require 'zlib'
+require 'tmpdir'
 
-o = {q:true, format: :json}
+o = {q: true, format: :json}
 OptionParser.new do |opt|
   opt_banner(opt)
   opt_object(opt, o, [:project])
   opt_filter_datasets(opt, o)
-  opt.on("-i", "--index FILE",
-    "Pre-calculated tax-index (in tabular format) to be used.",
-    "If passed, dataset filtering arguments are ignored."
-    ){ |v| o[:index]=v }
+  opt.on('-i', '--index FILE',
+    'Pre-calculated tax-index (in tabular format) to be used.',
+    'If passed, dataset filtering arguments are ignored.'
+    ){ |v| o[:index] = v }
   opt_common(opt, o)
 end.parse!
 
 ##=> Functions <=
 # Returns the _cannonical_ ID between strings +a+ and +b+.
-def cannid(a, b) ; [a, b].sort.join("-") ; end
+def cannid(a, b) ; [a, b].sort.join('-') ; end
 
 ##=> Main <=
-opt_require(o, project:"-P")
+opt_require(o, project: '-P')
 
-$stderr.puts "Loading project." unless o[:q]
+$stderr.puts 'Loading project.' unless o[:q]
 p = MiGA::Project.load(o[:project])
 raise "Impossible to load project: #{o[:project]}" if p.nil?
 
-metric = p.is_clade? ? "ani" : "aai"
+metric = p.is_clade? ? 'ani' : 'aai'
 res_n  = "#{metric}_distances"
 $stderr.puts "Reading distances (1-#{metric.upcase})." unless o[:q]
 res = p.result res_n
@@ -38,31 +38,31 @@ raise "#{res_n} not yet calculated." if res.nil?
 matrix = res.file_path(:matrix)
 raise "#{res_n} has no matrix." if matrix.nil?
 dist = {}
-mfh = matrix=~/\.gz$/ ? Zlib::GzipReader.open(matrix) : File.open(matrix,"r")
+mfh = matrix =~ /\.gz$/ ? Zlib::GzipReader.open(matrix) : File.open(matrix, 'r')
 mfh.each_line do |ln|
   next if mfh.lineno==1
   row = ln.chomp.split(/\t/)
-  dist[cannid(row[1], row[2])] = [row[3], 0, ["root:biota"]]
+  dist[cannid(row[1], row[2])] = [row[3], row[5], row[6], 0, ['root:biota']]
 end
 mfh.close
 
 Dir.mktmpdir do |dir|
   if o[:index].nil?
-    $stderr.puts "Loading datasets." unless o[:q]
+    $stderr.puts 'Loading datasets.' unless o[:q]
     ds = p.datasets
     ds.select!{ |d| not d.metadata[:tax].nil? }
     ds = filter_datasets!(ds, o)
     
-    $stderr.puts "Indexing taxonomy." unless o[:q]
+    $stderr.puts 'Indexing taxonomy.' unless o[:q]
     tax_index = MiGA::TaxIndex.new
     ds.each { |d| tax_index << d }
-    tab = File.expand_path("index.tab", dir)
-    File.open(tab, "w") { |fh| fh.print tax_index.to_tab }
+    tab = File.expand_path('index.tab', dir)
+    File.open(tab, 'w') { |fh| fh.print tax_index.to_tab }
   else
     tab = o[:index]
   end
-  
-  $stderr.puts "Traversing taxonomy." unless o[:q]
+
+  $stderr.puts 'Traversing taxonomy.' unless o[:q]
   rank_i = 0
   MiGA::Taxonomy.KNOWN_RANKS.each do |rank|
     $stderr.print "o #{rank}: " unless o[:q]
@@ -70,13 +70,13 @@ Dir.mktmpdir do |dir|
     rank_i += 1
     in_rank = nil
     ds_name = []
-    File.open(tab, "r") do |fh|
+    File.open(tab, 'r') do |fh|
       fh.each_line do |ln|
         if ln =~ /^ {#{(rank_i-1)*2}}\S+:\S+:/
           in_rank = nil
           ds_name = []
         elsif ln =~ /^ {#{rank_i*2}}(#{rank}:(\S+)):/
-          in_rank = $2=="?" ? nil : $1
+          in_rank = $2 == '?' ? nil : $1
           ds_name = []
         elsif ln =~ /^ *# (\S+)/ and not in_rank.nil?
           ds_i = $1
@@ -85,8 +85,8 @@ Dir.mktmpdir do |dir|
             k = cannid(ds_i, ds_j)
             next if dist[k].nil?
             rank_n += 1
-            dist[k][1] = rank_i
-            dist[k][2].unshift in_rank
+            dist[k][3] = rank_i
+            dist[k][4].unshift in_rank
           end
         end
       end
@@ -95,6 +95,10 @@ Dir.mktmpdir do |dir|
   end
 end
 
+$stderr.puts 'Generating report.' unless o[:q]
 dist.keys.each do |k|
-  puts (k.split("-") + dist[k].flatten).join("\t")
+  dist[k][5] = dist[k][4].join(' ')
+  dist[k][4] = dist[k][4][1]
+  puts (k.split('-') + dist[k]).join("\t")
 end
+
--- utils/plot-taxdist.R
+++ utils/plot-taxdist.R
@@ -7,8 +7,7 @@
 # To update the AAI data files, use:
 # 
 # ```bash
-# miga tax_distributions -P /Path/To/RefSeq --ref | cut -f 1-5 \
-#   > aai-tax-index.tsv
+# miga tax_dist -P /Path/To/TypeMat --ref > aai-tax-index.tsv
 # ```
 # 
 # Next, in R:
@@ -16,12 +15,12 @@
 # ```R
 # source("utils/plot-taxdist.R")
 # p.val <- plot.miga.taxdist("aai-tax-index.tsv",
-#   exclude=c("g:Mycoplasma", "ssp:Prochlorococcus_marinus_subsp__marinus",
+#   exclude = c("g:Mycoplasma", "ssp:Prochlorococcus_marinus_subsp__marinus",
 #   "f:Rhizobiaceae", "s:Buchnera_aphidicola", "s:Prochlorococcus_marinus"))
-# write.table(p.val[[1]], file="lib/miga/_data/aai-intax.tsv",
-#   sep="\t", row.names=TRUE, col.names=NA, quote=FALSE)
-# write.table(p.val[[2]], file="lib/miga/_data/aai-novel.tsv",
-#   sep="\t", row.names=TRUE, col.names=NA, quote=FALSE)
+# write.table(p.val[[1]], file = "lib/miga/_data/aai-intax.tsv",
+#   sep = "\t", row.names = TRUE, col.names = NA, quote = FALSE)
+# write.table(p.val[[2]], file = "lib/miga/_data/aai-novel.tsv",
+#   sep = "\t", row.names = TRUE, col.names = NA, quote = FALSE)
 # ```
 #
 # And finally, back in bash:
@@ -37,55 +36,64 @@
 #argv <- commandArgs(trailingOnly=T)
 
 #= Functions
-plot.miga.taxdist <- function(file, exclude=c()){
-  pdf(paste(file, ".pdf", sep=""), 6, 7)
+plot.miga.taxdist <- function(file, exclude = c(), format = 'pdf'){
+  if(format == 'pdf'){
+    pdf(paste(file, '.pdf', sep = ''), 6, 7)
+  }else if(format == 'png'){
+    png(paste(file, '.png', sep = ''), 480, 516)
+  }else{
+    stop('Supported formats: "pdf" and "png".')
+  }
   layout(1:3, heights=c(2,1,1.5))
-  d <- read.table(file, sep="\t", header=FALSE,
-    col.names=c("a","b","aai","rank","taxon"), as.is=TRUE)
+  d <- read.table(file, sep = "\t", header = FALSE, as.is = TRUE,
+    col.names=c('a', 'b', 'aai', 'n', 'omega', 'rank', 'taxon', 'taxonomy'))
   a <- d[!(d$taxon %in% exclude),]
-  col <- rainbow(max(a$rank)+1, s=3/4, v=3/4, alpha=1/3)
-  col2 <- rainbow(max(a$rank)+1, s=3/4, v=3/4)
+  col  <- rainbow(max(a$rank)+1, s = 3/4, v = 3/4, alpha = 1/3)
+  col2 <- rainbow(max(a$rank)+1, s = 3/4, v = 3/4)
 
   cat("o Plot pairs.\n")
   par(mar=c(0,4,1,1)+0.1)
-  plot(d$aai, d$rank+runif(nrow(d), -0.3, 0.3), cex=1/2, pch=16, las=1, bty="l",
-    col=ifelse(d$taxon %in% exclude, rgb(.8,.8,.8,1/4), col[d$rank+1]),
-    xlab="", ylab="Lowest common taxon", xaxt="n", ylim=rev(range(a$rank)))
+  plot(d$aai, d$rank+runif(nrow(d), -0.3, 0.3),
+    cex = 1/2, pch = 16, las = 1, bty = "l",
+    col = ifelse(d$taxon %in% exclude, rgb(.8,.8,.8,1/4), col[d$rank+1]),
+    xlab = "", ylab = "Lowest common taxon",
+    xaxt = "n", ylim = rev(range(a$rank)))
   for(i in c(0.1, 0.05, 0.01)){
     min_q <- tapply(a$aai, a$rank, quantile, probs=i)
     max_q <- tapply(a$aai, a$rank, quantile, probs=1-i)
-    arrows(x0=min_q, length=0, col=grey(1+log10(i)/2),
-      y0=as.numeric(names(min_q))-0.45, y1=as.numeric(names(min_q))+0.45)
-    arrows(x0=max_q, length=0, col=grey(1+log10(i)/2),
-      y0=as.numeric(names(max_q))-0.45, y1=as.numeric(names(max_q))+0.45)
+    arrows(x0 = min_q, length = 0, col = grey(1+log10(i)/2),
+      y0 = as.numeric(names(min_q))-0.45, y1 = as.numeric(names(min_q))+0.45)
+    arrows(x0 = max_q, length = 0, col = grey(1+log10(i)/2),
+      y0 = as.numeric(names(max_q))-0.45, y1 = as.numeric(names(max_q))+0.45)
   }
   
   cat("o Plot taxa.\n")
-  par(mar=c(0,4,0,1)+0.1)
-  plot(1, type="n", xlim=range(a$aai), ylim=rev(range(a$rank)),
-    xlab="", ylab="Lowest common taxon", xaxt="n", las=1, bty="l")
+  par(mar = c(0,4,0,1)+0.1)
+  plot(1, type = "n", xlim = range(a$aai), ylim = rev(range(a$rank)),
+    xlab = "", ylab = "Lowest common taxon", xaxt = "n", las = 1, bty = "l")
   for(i in unique(a$rank)){
     t.aai <- tapply(d$aai[d$rank==i], d$taxon[d$rank==i], mean)
     t.size <- tapply(d$aai[d$rank==i], d$taxon[d$rank==i], length)
-    points(t.aai, i+runif(length(t.aai), -0.15, 0.15), pch=16,
-      cex=2*log2(1+t.size)/log2(1+max(t.size)),
-      col=ifelse(names(t.aai) %in% exclude, rgb(.8,.8,.8,1/4), col[i+1]))
+    points(t.aai, i+runif(length(t.aai), -0.15, 0.15), pch = 16,
+      cex = 2*log2(1+t.size)/log2(1+max(t.size)),
+      col = ifelse(names(t.aai) %in% exclude, rgb(.8,.8,.8,1/4), col[i+1]))
   }
   
   cat("o Plot p-values.\n")
-  par(mar=c(4,4,0,1)+0.1)
-  plot(1, type="n", xlim=range(a$aai), ylim=c(0,0.5),
-    xlab="AAI (%)", ylab="P-value (--- intax; - - novel)", las=1, bty="l")
+  par(mar = c(4,4,0,1)+0.1)
+  plot(1, type = "n", xlim = range(a$aai), ylim = c(0,0.5),
+    xlab = "AAI (%)", ylab = "P-value (--- intax; - - novel)",
+    las = 1, bty = "l")
   x <- seq(30, 100, 0.1)
-  intax <- data.frame(row.names=x)
-  novel <- data.frame(row.names=x)
+  intax <- data.frame(row.names = x)
+  novel <- data.frame(row.names = x)
   for(i in sort(unique(a$rank))){
     if(i==12) next
     k <- as.character(i)
     intax[,k] <- miga.taxprob.intax(x, i, a)
     novel[,k] <- miga.taxprob.novel(x, i, a)
-    lines(x, intax[,k], col=col2[i+1], lwd=2)
-    lines(x, novel[,k], col=col2[i+1], lwd=2, lty=2)
+    lines(x, intax[,k], col = col2[i+1], lwd = 2)
+    lines(x, novel[,k], col = col2[i+1], lwd = 2, lty = 2)
   }
   dev.off()
   return(list(intax, novel))
@@ -106,3 +114,4 @@ miga.taxprob.intax <- function(max.aai, rank, data){
   }
   return(o)
 }
+
