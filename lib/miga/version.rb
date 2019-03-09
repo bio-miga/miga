170468fbb259b286e95b348e1785c512ceaed377
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-03-08 20:41:07 -0500

    Tiny version bump: 0.3.9.1

--- lib/miga/version.rb
+++ lib/miga/version.rb
@@ -10,7 +10,7 @@ module MiGA
   # - Float representing the major.minor version.
   # - Integer representing gem releases of the current version.
   # - Integer representing minor changes that require new version number.
-  VERSION = [0.3, 9, 0]
+  VERSION = [0.3, 9, 1]
   
   ##
   # Nickname for the current major.minor version.
@@ -18,7 +18,7 @@ module MiGA
   
   ##
   # Date of the current gem release.
-  VERSION_DATE = Date.new(2019, 03, 07)
+  VERSION_DATE = Date.new(2019, 03, 8)
   
   ##
   # Reference of MiGA.
