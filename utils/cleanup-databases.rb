9c4b54699ce6402f895f325075abf7afa516aae0
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-03-15 16:31:40 -0400

    Adds advance reporting to cleanup-databases util

--- utils/cleanup-databases.rb
+++ utils/cleanup-databases.rb
@@ -5,17 +5,22 @@ require 'miga'
 
 ARGV[1] or abort "Usage: #{$0} path/to/project threads"
 
-$stderr.puts "Cleaning databases..."
+$stderr.puts 'Cleaning databases...'
 p = MiGA::Project.load(ARGV[0])
 ds_names = p.dataset_names
 thr = ARGV[1].to_i
 
+pc = [0] + (1 .. 100).map{ |i| ds_names.size * i / 100 }
+$stderr.puts (('.'*9 + '|')*10) + ' 100%'
+
 (0 .. thr-1).each do |t|
   fork do
-    k = -1
-    ds_names.each do |i|
-      k = (k+1) % thr
-      next unless k == t
+    ds_names.each_with_index do |i, idx|
+      while t == 0 and idx+1 > pc.first
+        $stderr.print '#'
+        pc.shift
+      end
+      next unless (idx % thr) == t
       d = p.dataset(i)
       next unless d.is_ref? and d.is_active?
       d.cleanup_distances!
@@ -23,4 +28,5 @@ thr = ARGV[1].to_i
   end
 end
 Process.waitall
+$stderr.puts ' Done'
 
