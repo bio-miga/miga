d2fc014dfaa582cae2f265d854309ff001531065
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-09-20 10:13:51 -0400

    Code cleanup for miga tax_dist

--- lib/miga/cli/action/tax_dist.rb
+++ lib/miga/cli/action/tax_dist.rb
@@ -25,6 +25,23 @@ class MiGA::Cli::Action::TaxDist < MiGA::Cli::Action
   end
 
   def perform
+    dist = read_distances
+    Dir.mktmpdir do |dir|
+      tab = get_tab_index(dir)
+      dist = traverse_taxonomy(tab, dist)
+    end
+
+    cli.say 'Generating report'
+    dist.keys.each do |k|
+      dist[k][5] = dist[k][4].reverse.join(' ')
+      dist[k][4] = dist[k][4].first
+      puts (k.split('-') + dist[k]).join("\t")
+    end
+  end
+
+  private
+
+  def read_distances
     p = cli.load_project
     metric = p.is_clade? ? 'ani' : 'aai'
     res_n  = "#{metric}_distances"
@@ -44,59 +61,57 @@ class MiGA::Cli::Action::TaxDist < MiGA::Cli::Action
     end
     cli.say "  Lines: #{mfh.lineno}"
     mfh.close
+    dist
+  end
 
-    Dir.mktmpdir do |dir|
-      if cli[:index].nil?
-        ds = cli.load_and_filter_datasets
-        ds.keep_if { |d| !d.metadata[:tax].nil? }
+  def get_tab_index(dir)
+    if cli[:index].nil?
+      ds = cli.load_and_filter_datasets
+      ds.keep_if { |d| !d.metadata[:tax].nil? }
 
-        cli.say 'Indexing taxonomy'
-        tax_index = TaxIndex.new
-        ds.each { |d| tax_index << d }
-        tab = File.expand_path('index.tab', dir)
-        File.open(tab, 'w') { |fh| fh.print tax_index.to_tab }
-      else
-        tab = cli[:index]
-      end
+      cli.say 'Indexing taxonomy'
+      tax_index = TaxIndex.new
+      ds.each { |d| tax_index << d }
+      tab = File.expand_path('index.tab', dir)
+      File.open(tab, 'w') { |fh| fh.print tax_index.to_tab }
+    else
+      tab = cli[:index]
+    end
+    tab
+  end
 
-      cli.say 'Traversing taxonomy'
-      rank_i = 0
-      Taxonomy.KNOWN_RANKS.each do |rank|
-        cli.say "o #{rank}: "
-        rank_n = 0
-        rank_i += 1
-        in_rank = nil
-        ds_name = []
-        File.open(tab, 'r') do |fh|
-          fh.each_line do |ln|
-            if ln =~ /^ {#{(rank_i-1)*2}}\S+:\S+:/
-              in_rank = nil
-              ds_name = []
-            elsif ln =~ /^ {#{rank_i*2}}(#{rank}:(\S+)):/
-              in_rank = $2 == '?' ? nil : $1
-              ds_name = []
-            elsif ln =~ /^ *# (\S+)/ and not in_rank.nil?
-              ds_i = $1
-              ds_name << ds_i
-              ds_name.each do |ds_j|
-                k = cannid(ds_i, ds_j)
-                next if dist[k].nil?
-                rank_n += 1
-                dist[k][3] = rank_i
-                dist[k][4].unshift in_rank
-              end
+  def traverse_taxonomy(tab, dist)
+    cli.say 'Traversing taxonomy'
+    rank_i = 0
+    Taxonomy.KNOWN_RANKS.each do |rank|
+      cli.say "o #{rank}: "
+      rank_n = 0
+      rank_i += 1
+      in_rank = nil
+      ds_name = []
+      File.open(tab, 'r') do |fh|
+        fh.each_line do |ln|
+          if ln =~ /^ {#{(rank_i-1)*2}}\S+:\S+:/
+            in_rank = nil
+            ds_name = []
+          elsif ln =~ /^ {#{rank_i*2}}(#{rank}:(\S+)):/
+            in_rank = $2 == '?' ? nil : $1
+            ds_name = []
+          elsif ln =~ /^ *# (\S+)/ and not in_rank.nil?
+            ds_i = $1
+            ds_name << ds_i
+            ds_name.each do |ds_j|
+              k = cannid(ds_i, ds_j)
+              next if dist[k].nil?
+              rank_n += 1
+              dist[k][3] = rank_i
+              dist[k][4].unshift in_rank
             end
           end
         end
-        cli.say "#{rank_n} pairs of datasets"
       end
+      cli.say "#{rank_n} pairs of datasets"
     end
-
-    cli.say 'Generating report'
-    dist.keys.each do |k|
-      dist[k][5] = dist[k][4].reverse.join(' ')
-      dist[k][4] = dist[k][4].first
-      puts (k.split('-') + dist[k]).join("\t")
-    end
+    dist
   end
 end
