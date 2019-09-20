c9cfb69cd8fcfeb440910309d114471be5f3d4d0
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-09-20 11:44:52 -0400

    Code cleanup for miga init

--- lib/miga/cli/action/init.rb
+++ lib/miga/cli/action/init.rb
@@ -5,33 +5,35 @@ require 'miga/cli/action'
 require 'shellwords'
 
 class MiGA::Cli::Action::Init < MiGA::Cli::Action
-
+  require 'miga/cli/action/init/daemon_helper'
+  include MiGA::Cli::Action::Init::DaemonHelper
+  
   def parse_cli
     cli.interactive = true
-    cli.defaults = {mytaxa: nil,
+    cli.defaults = { mytaxa: nil,
       config: File.expand_path('.miga_modules', ENV['HOME']),
-      ask: false, auto: false, dtype: :bash}
+      ask: false, auto: false, dtype: :bash }
     cli.parse do |opt|
       opt.on(
         '-c', '--config PATH',
         'Path to the Bash configuration file',
         "By default: #{cli[:config]}"
-        ){ |v| cli[:config] = v }
+      ){ |v| cli[:config] = v }
       opt.on(
         '--[no-]mytaxa',
         'Should I try setting up MyTaxa its dependencies?',
         'By default: interactive (true if --auto)'
-        ){ |v| cli[:mytaxa] = v }
+      ){ |v| cli[:mytaxa] = v }
       opt.on(
         '--daemon-type STRING',
         'Type of daemon launcher, one of: bash, qsub, msub, slurm',
         "By default: interactive (#{cli[:dtype]} if --auto)"
-        ){ |v| cli[:dtype] = v.to_sym }
+      ){ |v| cli[:dtype] = v.to_sym }
       opt.on(
         '--ask-all',
         'Ask for the location of all software',
         'By default, only the locations missing in PATH are requested'
-        ){ |v| cli[:ask] = v }
+      ){ |v| cli[:ask] = v }
     end
   end
 
@@ -144,11 +146,7 @@ BASH
 
   def check_software_requirements(rc_fh)
     cli.puts 'Looking for requirements:'
-    if cli[:mytaxa].nil?
-      cli[:mytaxa] = cli.ask_user(
-            'Should I include MyTaxa modules?',
-            'yes', %w(yes no)) == 'yes'
-    end
+    ask_for_mytaxa
     rc_fh.puts 'export MIGA_MYTAXA="no"' unless cli[:mytaxa]
     paths = {}
     rc_fh.puts 'MIGA_PATH=""'
@@ -159,29 +157,44 @@ BASH
         r = ln.chomp.split(/\t+/)
         next if r[0] =~ /\(opt\)$/ && !cli[:mytaxa]
         cli.print "Testing #{r[0]}#{" (#{r[3]})" if r[3]}... "
-        path = nil
-        loop do
-          d_path = File.dirname(run_cmd(cli, "which #{r[1].shellescape}"))
-          if cli[:ask] || d_path == '.'
-            path = cli.ask_user('Where can I find it?', d_path, nil, true)
-          else
-            path = d_path
-            cli.puts path
-          end
-          if File.executable?(File.expand_path(r[1], path))
-            if d_path != path
-              rc_fh.puts "MIGA_PATH=\"#{path}:$MIGA_PATH\" # #{r[1]}"
-            end
-            break
-          end
-          cli.print "I cannot find #{r[1]} "
-        end
+        path = find_software(r[1])
         paths[r[1]] = File.expand_path(r[1], path).shellescape
       end
     end
     rc_fh.puts 'export PATH="$MIGA_PATH$PATH"'
     cli.puts ''
     paths
+  end
+
+  def ask_for_mytaxa
+    if cli[:mytaxa].nil?
+      cli[:mytaxa] =
+        cli.ask_user(
+          'Should I include MyTaxa modules?',
+          'yes', %w(yes no)
+        ) == 'yes'
+    end
+  end
+
+  def find_software(exec)
+    path = nil
+    loop do
+      d_path = File.dirname(run_cmd(cli, "which #{exec.shellescape}"))
+      if cli[:ask] || d_path == '.'
+        path = cli.ask_user('Where can I find it?', d_path, nil, true)
+      else
+        path = d_path
+        cli.puts path
+      end
+      if File.executable?(File.expand_path(exec, path))
+        if d_path != path
+          rc_fh.puts "MIGA_PATH=\"#{path}:$MIGA_PATH\" # #{exec}"
+        end
+        break
+      end
+      cli.print "I cannot find #{exec} "
+    end
+    path
   end
 
   def check_additional_files(paths)
@@ -240,123 +253,6 @@ BASH
       end
     end
     cli.puts ''
-  end
-
-  def configure_daemon
-    cli.puts 'Default daemon configuration:'
-    daemon_f = File.expand_path('.miga_daemon.json', ENV['HOME'])
-    unless File.exist?(daemon_f) and cli.ask_user(
-              'A template daemon already exists, do you want to preserve it?',
-              'yes', %w(yes no)) == 'yes'
-      v = {created: Time.now.to_s, updated: Time.now.to_s}
-      v[:type] = cli.ask_user(
-        'Please select the type of daemon you want to setup',
-        cli[:dtype], %w(bash qsub msub slurm))
-      case v[:type]
-      when 'bash'
-        v = configure_bash_daemon(v)
-      when 'slurm'
-        v = configure_slurm_daemon(v)
-      else # [qm]sub
-        v = configure_qsub_msub_daemon(v)
-      end
-      File.open(daemon_f, 'w') { |fh| fh.puts JSON.pretty_generate(v) }
-    end
-    cli.puts ''
-  end
-
-  def configure_bash_daemon(v)
-    v[:latency] = cli.ask_user('How long should I sleep? (in secs)', '30').to_i
-    v[:maxjobs] = cli.ask_user('How many jobs can I launch at once?', '6').to_i
-    v[:ppn]     = cli.ask_user('How many CPUs can I use per job?', '2').to_i
-    cli.puts 'Setting up internal daemon defaults.'
-    cli.puts 'If you don\'t understand this just leave default values:'
-    v[:cmd]     = cli.ask_user(
-      "How should I launch tasks?\n  %1$s: script path, " \
-        "%2$s: variables, %3$d: CPUs, %4$s: log file, %5$s: task name.\n",
-      "%2$s '%1$s' > '%4$s' 2>&1")
-    v[:var]     = cli.ask_user(
-      "How should I pass variables?\n  %1$s: keys, %2$s: values.\n",
-      "%1$s=%2$s")
-    v[:varsep]  = cli.ask_user('What should I use to separate variables?', ' ')
-    v[:alive]   = cli.ask_user(
-      "How can I know that a process is still alive?\n  %1$s: PID, " \
-        "output should be 1 for running and 0 for non-running.\n",
-      "ps -p '%1$s'|tail -n+2|wc -l")
-    v[:kill]    = cli.ask_user(
-      "How should I terminate tasks?\n  %s: process ID.", "kill -9 '%s'")
-    v
-  end
-
-  def configure_slurm_daemon(v)
-    queue       = cli.ask_user('What queue should I use?', nil, nil, true)
-    v[:latency] = cli.ask_user('How long should I sleep? (in secs)', '150').to_i
-    v[:maxjobs] = cli.ask_user('How many jobs can I launch at once?', '300').to_i
-    v[:ppn]     = cli.ask_user('How many CPUs can I use per job?', '2').to_i
-    cli.puts 'Setting up internal daemon defaults'
-    cli.puts 'If you don\'t understand this just leave default values:'
-    v[:cmd]     = cli.ask_user(
-      "How should I launch tasks?\n  %1$s: script path, " \
-        "%2$s: variables, %3$d: CPUs, %4$d: log file, %5$s: task name.\n",
-      "%2$s sbatch --partition='#{queue}' --export=ALL " \
-        "--nodes=1 --ntasks-per-node=%3$d --output='%4$s' " \
-        "--job-name='%5$s' --mem=9G --time=12:00:00 %1$s " \
-        "| perl -pe 's/.* //'")
-    v[:var]     = cli.ask_user(
-      "How should I pass variables?\n  %1$s: keys, %2$s: values.\n",
-      "%1$s=%2$s")
-    v[:varsep]  = cli.ask_user(
-      'What should I use to separate variables?', ' ')
-    v[:alive]   = cli.ask_user(
-      "How can I know that a process is still alive?\n  %1$s: job id, " \
-        "output should be 1 for running and 0 for non-running.\n",
-      "squeue -h -o %%t -j '%1$s' | grep '^PD\\|R\\|CF\\|CG$' " \
-        "| tail -n 1 | wc -l")
-    v[:kill]    = cli.ask_user(
-      "How should I terminate tasks?\n  %s: process ID.", "scancel '%s'")
-    v
-  end
-
-  def configure_qsub_msub_daemon
-    queue       = cli.ask_user('What queue should I use?', nil, nil, true)
-    v[:latency] = cli.ask_user('How long should I sleep? (in secs)', '150').to_i
-    v[:maxjobs] = cli.ask_user('How many jobs can I launch at once?', '300').to_i
-    v[:ppn]     = cli.ask_user('How many CPUs can I use per job?', '2').to_i
-    cli.puts 'Setting up internal daemon defaults.'
-    cli.puts 'If you don\'t understand this just leave default values:'
-    v[:cmd]     = cli.ask_user(
-      "How should I launch tasks?\n  %1$s: script path, " \
-        "%2$s: variables, %3$d: CPUs, %4$d: log file, %5$s: task name.\n",
-      "#{v[:type]} -q '#{queue}' -v '%2$s' -l nodes=1:ppn=%3$d %1$s " \
-        "-j oe -o '%4$s' -N '%5$s' -l mem=9g -l walltime=12:00:00 " \
-        "| grep .")
-    v[:var]     = cli.ask_user(
-      "How should I pass variables?\n  %1$s: keys, %2$s: values.\n",
-      "%1$s=%2$s")
-    v[:varsep]  = cli.ask_user(
-      'What should I use to separate variables?', ',')
-    if v[:type] == 'qsub'
-      v[:alive] = cli.ask_user(
-        "How can I know that a process is still alive?\n  " \
-          "%1$s: job id, output should be 1 for running and " \
-          "0 for non-running.\n",
-        "qstat -f '%1$s'|grep ' job_state ='|perl -pe 's/.*= //'" \
-          "|grep '[^C]'|tail -n1|wc -l|awk '{print $1}'")
-      v[:kill]  = cli.ask_user(
-        "How should I terminate tasks?\n  %s: process ID.", "qdel '%s'")
-    else # msub
-      v[:alive] = cli.ask_user(
-        "How can I know that a process is still alive?\n  " \
-          "%1$s: job id, output should be 1 for running and " \
-          "0 for non-running.\n",
-        "checkjob '%1$s'|grep '^State:'|perl -pe 's/.*: //'" \
-          "|grep 'Deferred\\|Hold\\|Idle\\|Starting\\|Running\\|Blocked'" \
-          "|tail -n1|wc -l|awk '{print $1}'")
-      v[:kill]  = cli.ask_user(
-        "How should I terminate tasks?\n  %s: process ID.",
-        "canceljob '%s'")
-    end
-    v
   end
 
   def close_rc_file(rc_fh)
---
+++ lib/miga/cli/action/init/daemon_helper.rb
@@ -0,0 +1,124 @@
+# @package MiGA
+# @license Artistic-2.0
+
+##
+# Helper module with daemon configuration functions for MiGA::Cli::Action::Init
+module MiGA::Cli::Action::Init::DaemonHelper
+  def configure_daemon
+    cli.puts 'Default daemon configuration:'
+    daemon_f = File.expand_path('.miga_daemon.json', ENV['HOME'])
+    unless File.exist?(daemon_f) and cli.ask_user(
+              'A template daemon already exists, do you want to preserve it?',
+              'yes', %w(yes no)) == 'yes'
+      v = {created: Time.now.to_s, updated: Time.now.to_s}
+      v[:type] = cli.ask_user(
+        'Please select the type of daemon you want to setup',
+        cli[:dtype], %w(bash qsub msub slurm))
+      case v[:type]
+      when 'bash'
+        v = configure_bash_daemon(v)
+      when 'slurm'
+        v = configure_slurm_daemon(v)
+      else # [qm]sub
+        v = configure_qsub_msub_daemon(v)
+      end
+      File.open(daemon_f, 'w') { |fh| fh.puts JSON.pretty_generate(v) }
+    end
+    cli.puts ''
+  end
+
+  def configure_bash_daemon(v)
+    v[:latency] = cli.ask_user('How long should I sleep? (in secs)', '30').to_i
+    v[:maxjobs] = cli.ask_user('How many jobs can I launch at once?', '6').to_i
+    v[:ppn]     = cli.ask_user('How many CPUs can I use per job?', '2').to_i
+    cli.puts 'Setting up internal daemon defaults.'
+    cli.puts 'If you don\'t understand this just leave default values:'
+    v[:cmd]     = cli.ask_user(
+      "How should I launch tasks?\n  %1$s: script path, " \
+        "%2$s: variables, %3$d: CPUs, %4$s: log file, %5$s: task name.\n",
+      "%2$s '%1$s' > '%4$s' 2>&1")
+    v[:var]     = cli.ask_user(
+      "How should I pass variables?\n  %1$s: keys, %2$s: values.\n",
+      "%1$s=%2$s")
+    v[:varsep]  = cli.ask_user('What should I use to separate variables?', ' ')
+    v[:alive]   = cli.ask_user(
+      "How can I know that a process is still alive?\n  %1$s: PID, " \
+        "output should be 1 for running and 0 for non-running.\n",
+      "ps -p '%1$s'|tail -n+2|wc -l")
+    v[:kill]    = cli.ask_user(
+      "How should I terminate tasks?\n  %s: process ID.", "kill -9 '%s'")
+    v
+  end
+
+  def configure_slurm_daemon(v)
+    queue       = cli.ask_user('What queue should I use?', nil, nil, true)
+    v[:latency] = cli.ask_user('How long should I sleep? (in secs)', '150').to_i
+    v[:maxjobs] = cli.ask_user('How many jobs can I launch at once?', '300').to_i
+    v[:ppn]     = cli.ask_user('How many CPUs can I use per job?', '2').to_i
+    cli.puts 'Setting up internal daemon defaults'
+    cli.puts 'If you don\'t understand this just leave default values:'
+    v[:cmd]     = cli.ask_user(
+      "How should I launch tasks?\n  %1$s: script path, " \
+        "%2$s: variables, %3$d: CPUs, %4$d: log file, %5$s: task name.\n",
+      "%2$s sbatch --partition='#{queue}' --export=ALL " \
+        "--nodes=1 --ntasks-per-node=%3$d --output='%4$s' " \
+        "--job-name='%5$s' --mem=9G --time=12:00:00 %1$s " \
+        "| perl -pe 's/.* //'")
+    v[:var]     = cli.ask_user(
+      "How should I pass variables?\n  %1$s: keys, %2$s: values.\n",
+      "%1$s=%2$s")
+    v[:varsep]  = cli.ask_user(
+      'What should I use to separate variables?', ' ')
+    v[:alive]   = cli.ask_user(
+      "How can I know that a process is still alive?\n  %1$s: job id, " \
+        "output should be 1 for running and 0 for non-running.\n",
+      "squeue -h -o %%t -j '%1$s' | grep '^PD\\|R\\|CF\\|CG$' " \
+        "| tail -n 1 | wc -l")
+    v[:kill]    = cli.ask_user(
+      "How should I terminate tasks?\n  %s: process ID.", "scancel '%s'")
+    v
+  end
+
+  def configure_qsub_msub_daemon
+    queue       = cli.ask_user('What queue should I use?', nil, nil, true)
+    v[:latency] = cli.ask_user('How long should I sleep? (in secs)', '150').to_i
+    v[:maxjobs] = cli.ask_user('How many jobs can I launch at once?', '300').to_i
+    v[:ppn]     = cli.ask_user('How many CPUs can I use per job?', '2').to_i
+    cli.puts 'Setting up internal daemon defaults.'
+    cli.puts 'If you don\'t understand this just leave default values:'
+    v[:cmd]     = cli.ask_user(
+      "How should I launch tasks?\n  %1$s: script path, " \
+        "%2$s: variables, %3$d: CPUs, %4$d: log file, %5$s: task name.\n",
+      "#{v[:type]} -q '#{queue}' -v '%2$s' -l nodes=1:ppn=%3$d %1$s " \
+        "-j oe -o '%4$s' -N '%5$s' -l mem=9g -l walltime=12:00:00 " \
+        "| grep .")
+    v[:var]     = cli.ask_user(
+      "How should I pass variables?\n  %1$s: keys, %2$s: values.\n",
+      "%1$s=%2$s")
+    v[:varsep]  = cli.ask_user(
+      'What should I use to separate variables?', ',')
+    if v[:type] == 'qsub'
+      v[:alive] = cli.ask_user(
+        "How can I know that a process is still alive?\n  " \
+          "%1$s: job id, output should be 1 for running and " \
+          "0 for non-running.\n",
+        "qstat -f '%1$s'|grep ' job_state ='|perl -pe 's/.*= //'" \
+          "|grep '[^C]'|tail -n1|wc -l|awk '{print $1}'")
+      v[:kill]  = cli.ask_user(
+        "How should I terminate tasks?\n  %s: process ID.", "qdel '%s'")
+    else # msub
+      v[:alive] = cli.ask_user(
+        "How can I know that a process is still alive?\n  " \
+          "%1$s: job id, output should be 1 for running and " \
+          "0 for non-running.\n",
+        "checkjob '%1$s'|grep '^State:'|perl -pe 's/.*: //'" \
+          "|grep 'Deferred\\|Hold\\|Idle\\|Starting\\|Running\\|Blocked'" \
+          "|tail -n1|wc -l|awk '{print $1}'")
+      v[:kill]  = cli.ask_user(
+        "How should I terminate tasks?\n  %s: process ID.",
+        "canceljob '%s'")
+    end
+    v
+  end
+
+end
