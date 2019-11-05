63abc28f2c51ff2ace0f41f484e519ead62508f4
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-11-05 15:39:55 -0500

    Functional workflow: miga quality_wf

--- lib/miga/cli/action/quality_wf.rb
+++ lib/miga/cli/action/quality_wf.rb
@@ -10,7 +10,8 @@ class MiGA::Cli::Action::QualityWf < MiGA::Cli::Action
   def parse_cli
     cli.expect_files = true
     cli.defaults = {
-      mytaxa: false, clean: false, regexp: MiGA::Cli.FILE_REGEXP }
+      mytaxa: false, clean: false, regexp: MiGA::Cli.FILE_REGEXP,
+      project_type: :genomes, dataset_type: :popgenome }
     cli.parse do |opt|
       opt.on(
         '-o', '--out_dir PATH',
@@ -29,15 +30,55 @@ class MiGA::Cli::Action::QualityWf < MiGA::Cli::Action
         'Regular expression indicating how to extract the name from the path',
         "By default: '#{cli[:regexp]}'"
       ) { |v| cli[:regexp] = v }
+      opt.on(
+        '-t', '--type STRING',
+        "Type of datasets. Recognized types include:",
+        *MiGA::Dataset.KNOWN_TYPES
+          .map { |k, v| "~ #{k}: #{v[:description]}" unless v[:multi] }
+      ) { |v| cli[:dataset_type] = v.downcase.to_sym }
       opts_for_wf(opt, 'Input genome assemblies (nucleotides, FastA)')
     end
   end
 
   def perform
+    # Input data
     p = create_project(:assembly)
-    # TODO: Add metadata flags to control workflow
-    # TODO: Run daemon
-    # TODO: Extract summaries
-    # TODO: Cleanup (if --clean)
+    # Customize pipeline
+    p.each_dataset do |d|
+      d.metadata[:run_mytaxa_scan] = false unless cli[:mytaxa]
+      d.metadata[:run_distances] = false
+      d.save
+    end
+    %w[
+      project_stats haai_distances aai_distances ani_distances clade_finding
+    ].each { |r| p.metadata["run_#{r}"] = false }
+    p.save
+    # Run
+    run_daemon
+    # Summarize
+    %w[cds assembly essential_genes ssu].each do |r|
+      call_cli([
+        'summary',
+        '-P', cli[:outdir],
+        '-r', r,
+        '-o', File.expand_path("#{r}.tsv", cli[:outdir]),
+        '--tab'
+      ])
+    end
+    if cli[:mytaxa]
+      dir = File.expand_path('mytaxa_scan', cli[:outdir])
+      Dir.mkdir(dir)
+      p.each_dataset do |d|
+        r = d.result(:mytaxa_scan) or next
+        f = r.file_path(:report) or next
+        FileUtils.cp(f, dir)
+      end
+    end
+    # Cleanup (if --clean)
+    if cli[:clean]
+      %w[data daemon metadata miga.project.json].each do |f|
+        FileUtils.rm_rf(File.expand_path(f, cli[:outdir]))
+      end
+    end
   end
 end
--- lib/miga/cli/action/wf.rb
+++ lib/miga/cli/action/wf.rb
@@ -11,19 +11,48 @@ module MiGA::Cli::Action::Wf
     opt.separator 'General Options'
     opt.on(
       '--daemon PATH',
-      'Use custom daemon configuration in JSON format'
-      ){ |v| cli[:daemon_json] = v }
+      'Use custom daemon configuration in JSON format',
+      'By default: ~/.miga_daemon.json'
+    ) { |v| cli[:daemon_json] = v }
+    opt.on(
+      '-t', '--threads INT',
+      'Number of parallel jobs to execute',
+      'By default controlled by the daemon configuration'
+    ) { |v| cli[:threads] = v.to_i }
   end
 
   def create_project(stage)
-    ensure_par(out_dir: '-o')
-    p = MiGA::Project.new(cli[:out_dir])
-    cli.files.each do |f|
-      m = cli[:regexp].match(f)
-      raise "Cannot extract name from file: #{f}" if m.nil? or m[1].nil?
-      name = m[1].miga_name
-      # TODO: Copy files and add to project (see `miga add`)
-    end
+    cli.ensure_par(
+      outdir: '-o',
+      project_type: '--project-type',
+      dataset_type: '--dataset-type')
+    # Create empty project
+    call_cli(['new', '-P', cli[:outdir], '-t', cli[:project_type]])
+    # Add datasets
+    call_cli([
+      'add',
+      '-P', cli[:outdir],
+      '-t', cli[:dataset_type],
+      '-i', stage,
+      '-R', cli[:regexp]
+    ] + cli.files)
+    p = MiGA::Project.load(cli[:outdir])
+    raise "Impossible to create project: #{cli[:outdir]}" if p.nil?
+    p
+  end
+
+  def call_cli(cmd)
+    cmd << '-v' if cli[:verbose]
+    MiGA::Cli.new(cmd.map(&:to_s)).launch
+  end
+
+  def run_daemon
+    cmd = ['daemon', 'run', '-P', cli[:outdir], '--shutdown-when-done']
+    cmd += ['--json', cli[:daemon_json]] unless cli[:daemon_json].nil?
+    cmd += ['--max-jobs', cli[:threads]] unless cli[:threads].nil?
+    cwd = Dir.pwd
+    call_cli cmd
+    Dir.chdir(cwd)
   end
 
 end
