603103cf32cd6e60b2f39df5f0e639c003452577
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-11-19 16:43:30 -0500

    New action: miga preproc_wf

---
+++ lib/miga/cli/action/preproc_wf.rb
@@ -0,0 +1,50 @@
+# @package MiGA
+# @license Artistic-2.0
+
+require 'miga/cli/action'
+
+class MiGA::Cli::Action::PreprocWf < MiGA::Cli::Action
+  require 'miga/cli/action/wf'
+  require 'miga/cli/action/add'
+  include MiGA::Cli::Action::Wf
+
+  def parse_cli
+    default_opts_for_wf
+    cli.defaults = { mytaxa: false }
+    cli.parse do |opt|
+      opt.on(
+        '-i', '--input-type STRING',
+        '(Mandatory) Type of input data, one of the following:',
+        *MiGA::Cli::Action::Add.INPUT_TYPES.map{ |k,v| "~ #{k}: #{v[0]}" }
+      ) { |v| cli[:input_type] = v.downcase.to_sym }
+      opt.on(
+        '-m', '--mytaxa_scan',
+        'Perform MyTaxa scan analysis'
+      ) { |v| cli[:mytaxa] = v }
+      opts_for_wf(opt, 'Input files as defined by --input-type', true, false)
+    end
+  end
+
+  def perform
+    # Input data
+    cli.ensure_par(input_type: '-i')
+    p = create_project(cli[:input_type])
+    # Customize pipeline
+    p.each_dataset do |d|
+      unless cli[:mytaxa]
+        d.metadata[:run_mytaxa_scan] = false
+        d.metadata[:run_mytaxa] = false
+      end
+      d.metadata[:run_distances] = false
+      d.save
+    end
+    %w[
+      project_stats haai_distances aai_distances ani_distances clade_finding
+    ].each { |r| p.metadata["run_#{r}"] = false }
+    p.save
+    # Run
+    run_daemon
+    # Summarize
+    summarize
+  end
+end
