ed1947060c5d18cb8a2e89f47145149f0251fefd
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2020-03-04 11:33:19 -0500

    miga run adapts to lingering dataset variables on -e

--- lib/miga/cli/action/run.rb
+++ lib/miga/cli/action/run.rb
@@ -31,33 +31,42 @@ class MiGA::Cli::Action::Run < MiGA::Cli::Action
   end
 
   def perform
+    # Load environment variables if requested (typically by the daemon)
     if cli[:env]
       cli[:project] ||= ENV['PROJECT']
       cli[:dataset] ||= ENV['DATASET']
       cli[:thr] ||= ENV['CORES'].to_i unless ENV['CORES'].nil?
       cli[:result] = File.basename(cli[:result].to_s, '.bash').to_sym
     end
-    virtual_task = false
-    miga = MiGA.root_path
+    
+    # Unset dataset if the requested result is for projects
+    if (MiGA::Project.RESULT_DIRS.keys + [:p]).include? cli[:result]
+      cli[:dataset] = nil
+    end
+    
+    # Load project
     p = cli.load_project
     
+    # Prepare command
+    miga = MiGA.root_path
     cmd = ["PROJECT=#{p.path.shellescape}",
       "RUNTYPE=#{cli[:remote] ? 'ssh' : 'bash'}",
       "MIGA=#{miga.shellescape}", "CORES=#{cli[:thr]}"]
     obj = cli.load_project_or_dataset
     klass = obj.class
-    virtual_task = true if [:p, :d].include? cli[:result]
+    virtual_task = [:p, :d].include?(cli[:result])
     cmd << "DATASET=#{obj.name.shellescape}" if obj.is_a? MiGA::Dataset
     if klass.RESULT_DIRS[cli[:result]].nil? and not virtual_task
       raise "Unsupported #{klass.to_s.sub(/.*::/, '')} result: #{cli[:result]}."
     end
     cmd << MiGA.script_path(cli[:result], miga: miga, project: p).shellescape
     if cli[:remote]
-      #cmd.unshift '.', '/etc/profile', ';'
       cmd = ['ssh', '-t', '-t', cli[:remote].shellescape,
         cmd.join(' ').shellescape]
     end
     cmd << ['>', cli[:log].shellescape, '2>&1'] if cli[:log]
+    
+    # Launch
     pid = spawn cmd.join(' ')
     Process.wait pid
   end
