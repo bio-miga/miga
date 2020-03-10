afbe368da92de85a674e9df45dc21f999a59f27b
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2020-03-10 14:40:59 -0400

    Fix precision on CLI advance

--- lib/miga/cli.rb
+++ lib/miga/cli.rb
@@ -119,7 +119,7 @@ class MiGA::Cli < MiGA::MiGA
   def advance(step, n = 0, total = nil, bin = true)
     return unless self[:verbose]
     adv = total.nil? ? (n == 0 ? '' : num_suffix(n, bin)) :
-      ('%.1f%% (%s/%s)' % [100 * n / total,
+      ('%.1f%% (%s/%s)' % [100.0 * n / total,
         num_suffix(n, bin), num_suffix(total, bin)])
     $stderr.print("[%s] %s %s    \r" % [Time.now, step, adv])
   end
