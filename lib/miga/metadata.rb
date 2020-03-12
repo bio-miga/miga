ed39fcd89e4af10d21162b6f2cb277eccfa3e36e
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2020-03-12 01:58:16 -0400

    Performance improvement
    - Minor code syle :up:
    - Performance improvement for `Dataset#[]=`. The gain is small, but this function is called many times over and takes the most time when loading datasets

--- lib/miga/metadata.rb
+++ lib/miga/metadata.rb
@@ -27,7 +27,7 @@ class MiGA::Metadata < MiGA::MiGA
 
   ##
   # Initiate a MiGA::Metadata object with description in +path+.
-  # It will create it if it doesn't exist
+  # It will create it if it doesn't exist.
   def initialize(path, defaults = {})
     @data = nil
     @path = File.absolute_path(path)
@@ -39,21 +39,21 @@ class MiGA::Metadata < MiGA::MiGA
   end
   
   ##
-  # Parsed data as a Hash.
+  # Parsed data as a Hash
   def data
     self.load if @data.nil?
     @data
   end
 
   ##
-  # Reset :created field and save the current data.
+  # Reset :created field and save the current data
   def create
     self[:created] = Time.now.to_s
     save
   end
 
   ##
-  # Save the metadata into #path.
+  # Save the metadata into #path
   def save
     MiGA.DEBUG "Metadata.save #{path}"
     self[:updated] = Time.now.to_s
@@ -78,7 +78,7 @@ class MiGA::Metadata < MiGA::MiGA
   end
 
   ##
-  # (Re-)load metadata stored in #path.
+  # (Re-)load metadata stored in #path
   def load
     sleeper = 0.0
     while File.exist? lock_file
@@ -91,7 +91,7 @@ class MiGA::Metadata < MiGA::MiGA
   end
 
   ##
-  # Delete file at #path.
+  # Delete file at #path
   def remove!
     MiGA.DEBUG "Metadata.remove! #{path}"
     File.unlink(path)
@@ -99,29 +99,41 @@ class MiGA::Metadata < MiGA::MiGA
   end
 
   ##
-  # Lock file for the metadata.
-  def lock_file ; "#{path}.lock" ; end
-
-  ##
-  # Return the value of +k+ in #data.
-  def [](k) data[k.to_sym] end
-
-  ##
-  # Set the value of +k+ to +v+.
-  def []=(k,v)
-    self.load if @data.nil?
-    k = k.to_sym
-    # Protect the special field :name
-    v=v.miga_name if k==:name
-    # Symbolize the special field :type
-    v=v.to_sym if k==:type
-    # Delete if nil, register, and return
-    v.nil? ? @data.delete(k) : (@data[k]=v)
+  # Lock file for the metadata
+  def lock_file
+    "#{path}.lock"
   end
 
   ##
-  # Iterate +blk+ for each data with 2 arguments key and value.
-  def each(&blk) data.each{ |k,v| blk.call(k,v) } ; end
+  # Return the value of +k+ in #data
+  def [](k)
+    data[k.to_sym]
+  end
+
+  ##
+  # Set the value of +k+ to +v+
+  def []=(k, v)
+    self.load if @data.nil?
+    k = k.to_sym
+    return @data.delete(k) if v.nil?
+
+    case k
+    when :name
+      # Protect the special field :name
+      v = v.miga_name
+    when :type
+      # Symbolize the special field :type
+      v = v.to_sym if k == :type
+    end
+
+    @data[k] = v
+  end
+
+  ##
+  # Iterate +blk+ for each data with 2 arguments: key and value
+  def each(&blk)
+    data.each { |k, v| blk.call(k, v) }
+  end
 
   ##
   # Show contents in JSON format as a String
