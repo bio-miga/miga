8462af5140af99dd82e4ca72bdbbac68338bd0ac
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2020-03-27 14:24:03 -0500

    New CLI option: miga new --fast
    Also, minor style code :up:

--- lib/miga/cli/action/new.rb
+++ lib/miga/cli/action/new.rb
@@ -11,20 +11,25 @@ class MiGA::Cli::Action::New < MiGA::Cli::Action
       opt.on(
         '-n', '--name STRING',
         'Name of the project'
-        ){ |v| cli[:name] = v }
+      ) { |v| cli[:name] = v }
       opt.on(
         '-d', '--description STRING',
         'Description of the project'
-        ){ |v| cli[:description] = v }
+      ) { |v| cli[:description] = v }
       opt.on(
         '-c', '--comments STRING',
         'Comments on the project'
-        ){ |v| cli[:comments] = v }
+      ) { |v| cli[:comments] = v }
+      opt.on(
+        '--fast',
+        'Use faster identity engines (Diamond-AAI and FastANI)',
+        'Equivalent to: -m aai_p=diamond,ani_p=fastani'
+      ) { |v| cli[:fast] = v }
       opt.on(
         '-m', '--metadata STRING',
         'Metadata as key-value pairs separated by = and delimited by comma',
         'Values are saved as strings except for booleans (true / false) or nil'
-        ){ |v| cli[:metadata] = v }
+      ) { |v| cli[:metadata] = v }
     end
   end
 
@@ -40,6 +45,10 @@ class MiGA::Cli::Action::New < MiGA::Cli::Action
     raise 'Project already exists, aborting.' if Project.exist? cli[:project]
     p = Project.new(cli[:project], false)
     p = cli.add_metadata(p)
+    if cli[:fast]
+      p.metadata[:aai_p] = 'diamond'
+      p.metadata[:ani_p] = 'fastani'
+    end
     p.save
   end
 end
