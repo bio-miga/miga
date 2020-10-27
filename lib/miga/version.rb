a3d3366868abd52d1b7f0c8b7ef3a4ae065a14ba
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2020-10-27 12:21:21 +0100

    Version bump to 0.7.16.2

--- lib/miga/version.rb
+++ lib/miga/version.rb
@@ -8,7 +8,7 @@ module MiGA
   # - Float representing the major.minor version.
   # - Integer representing gem releases of the current version.
   # - Integer representing minor changes that require new version number.
-  VERSION = [0.7, 16, 1]
+  VERSION = [0.7, 16, 2]
 
   ##
   # Nickname for the current major.minor version.
@@ -16,7 +16,7 @@ module MiGA
 
   ##
   # Date of the current gem release.
-  VERSION_DATE = Date.new(2020, 10, 19)
+  VERSION_DATE = Date.new(2020, 10, 27)
 
   ##
   # Reference of MiGA.
