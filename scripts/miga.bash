712548575866698b586b37cec04edc148f1e0599
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2020-12-28 22:21:12 +0100

    Improve logs

--- scripts/miga.bash
+++ scripts/miga.bash
@@ -11,17 +11,16 @@ function exists { [[ -e "$1" ]] ; }
 function fx_exists { [[ $(type -t "$1") == "function" ]] ; }
 
 if [[ "$SCRIPT" != "d" && "$SCRIPT" != "p" ]] ; then
-  echo '############'
-  echo -n "Date: " ; miga date
-  echo "Hostname: $(hostname)"
-  echo "MiGA: $MIGA"
-  echo "Task: $SCRIPT"
-  echo "Project: $PROJECT"
+  echo "######[ $SCRIPT ]######"
+  echo "# Date: $(miga date)"
+  echo "# Hostname: $(hostname)"
+  echo "# MiGA: $MIGA"
+  echo "# Project: $PROJECT"
   if [[ -n $DATASET ]] ; then
-    echo "Dataset: $DATASET"
+    echo "# Dataset: $DATASET"
     miga edit -P "$PROJECT" -D "$DATASET" -m "_step=$SCRIPT"
   fi
-  echo '------------'
+  echo '#------------'
 fi
 
 true
