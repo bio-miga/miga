c4f9e972f47f3068f69d7af6618e61f511aa393e
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2021-02-20 21:25:34 +0100

    Minor advance improvement in miga add

--- lib/miga/cli/action/add.rb
+++ lib/miga/cli/action/add.rb
@@ -170,7 +170,7 @@ class MiGA::Cli::Action::Add < MiGA::Cli::Action
     file_type[2].each_with_index do |ext, i|
       gz = file[i] =~ /\.gz/ ? '.gz' : ''
       FileUtils.cp(file[i], "#{r_path}#{ext}#{gz}")
-      cli.say "  file: #{file[i]}"
+      cli.say "  file: #{File.basename(file[i])}"
     end
     File.open("#{r_path}.done", 'w') { |f| f.print Time.now.to_s }
   end
