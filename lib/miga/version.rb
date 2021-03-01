5cf8afdfb31bf8f9208f76344a8f998081b19257
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2021-03-01 18:54:24 +0100

    Submodules to gem

--- lib/miga/version.rb
+++ lib/miga/version.rb
@@ -10,7 +10,7 @@ module MiGA
   # - Float representing the major.minor version.
   # - Integer representing gem releases of the current version.
   # - Integer representing minor changes that require new version number.
-  VERSION = [0.7, 26, 0].freeze
+  VERSION = [0.7, 26, 2].freeze
 
   ##
   # Nickname for the current major.minor version.
