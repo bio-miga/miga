e527254056cc3fbceee5e5ae7dc7e70b08d5e74f
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2021-10-28 23:13:23 +0200

    Relax ETA test slightly

--- test/common_test.rb
+++ test/common_test.rb
@@ -92,7 +92,7 @@ class CommonTest < Test::Unit::TestCase
     # Predict time
     sleep(1)
     o = capture_stderr { m.advance('x', 2, 1000) }.string
-    assert_match(/\] x 0\.2% \(2\/1000\) 16\.\dm left *\r/, o)
+    assert_match(/\] x 0\.2% \(2\/1000\) 1[5-7]\.\dm left *\r/, o)
   end
 
   def test_num_suffix
