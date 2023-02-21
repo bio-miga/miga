f80ce2250db581c51dd837d6fc27c41d4f00c7e0
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2023-02-21 14:04:55 +0100

    Clean enveomics

--- lib/miga/version.rb
+++ lib/miga/version.rb
@@ -12,7 +12,7 @@ module MiGA
   # - String indicating release status:
   #   - rc* release candidate, not released as gem
   #   - [0-9]+ stable release, released as gem
-  VERSION = [1.2, 17, 1].freeze
+  VERSION = [1.2, 17, 2].freeze
 
   ##
   # Nickname for the current major.minor version.
@@ -20,7 +20,7 @@ module MiGA
 
   ##
   # Date of the current gem relese.
-  VERSION_DATE = Date.new(2023, 2, 14)
+  VERSION_DATE = Date.new(2023, 2, 21)
 
   ##
   # References of MiGA
