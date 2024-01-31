742530841ea32018c265b217ed851d00135f9f8a
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-01-31 16:01:57 +0100

    Update version.rb

--- lib/miga/version.rb
+++ lib/miga/version.rb
@@ -12,7 +12,7 @@ module MiGA
   # - String indicating release status:
   #   - rc* release candidate, not released as gem
   #   - [0-9]+ stable release, released as gem
-  VERSION = [1.3, 9, 8].freeze
+  VERSION = [1.3, 10, 0].freeze
 
   ##
   # Nickname for the current major.minor version.
@@ -20,7 +20,7 @@ module MiGA
 
   ##
   # Date of the current gem relese.
-  VERSION_DATE = Date.new(2024, 1, 30)
+  VERSION_DATE = Date.new(2024, 1, 31)
 
   ##
   # References of MiGA
