384bcffeabc904d906e7a1412e4328826ae7ec47
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-06-23 18:37:05 +0200

    Load dataset stats if existing

--- scripts/stats.bash
+++ scripts/stats.bash
@@ -15,7 +15,8 @@ miga date > "${DATASET}.start"
 for i in raw_reads trimmed_fasta assembly \
          cds essential_genes distances taxonomy ssu ; do
   echo "# $i"
-  miga stats --compute-and-save --ignore-empty -P "$PROJECT" -D "$DATASET" -r $i
+  miga stats --compute-and-save --try-load --ignore-empty \
+    -P "$PROJECT" -D "$DATASET" -r "$i"
 done
 
 # Finalize
